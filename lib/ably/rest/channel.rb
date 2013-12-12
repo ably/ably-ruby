00dadf20e3eb32b9db20eda88194c41f7c51c552
Lewis Marshall <lewis@lmars.net>
2013-12-12 18:45:39 +0000

    Add some documentation

--- lib/ably/rest/channel.rb
+++ lib/ably/rest/channel.rb
@@ -3,11 +3,19 @@ module Ably
     class Channel
       attr_reader :client, :name
 
+      # Initialize a new Channel object
+      #
+      # @param client [Ably::Rest::Client]
+      # @param name [String] The name of the channel
       def initialize(client, name)
         @client = client
         @name   = name
       end
 
+      # Publish a message to the channel
+      #
+      # @param message [Hash] The message to publish
+      # @return [Boolean] true if the message was published, otherwise false
       def publish(message)
         response = client.post("/channels/#{name}/publish", message)
 
--- lib/ably/rest/client.rb
+++ lib/ably/rest/client.rb
@@ -6,19 +6,26 @@ require "ably/rest/middleware/parse_json"
 
 module Ably
   module Rest
+    # Wrapper for the Ably REST API
     class Client
       def initialize(options)
         @api_key = options[:api_key]
       end
 
+      # Perform an HTTP GET request to the API
       def get(path)
         request(:get, path)
       end
 
+      # Perform an HTTP POST request to the API
       def post(path, params)
         request(:post, path, params)
       end
 
+      # Return a Channel for the given name
+      #
+      # @param name [String] The name of the channel
+      # @return [Ably::Rest::Channel]
       def channel(name)
         @channels ||= {}
         @channels[name] ||= Channel.new(self, name)
@@ -31,10 +38,16 @@ module Ably
         end
       end
 
+      # Return a Faraday::Connection to use to make HTTP requests
+      #
+      # @return [Faraday::Connection]
       def connection
         @connection ||= Faraday.new(Ably::Rest.api_endpoint, connection_options)
       end
 
+      # Return a Hash of connection options to initiate the Faraday::Connection with
+      #
+      # @return [Hash]
       def connection_options
         @connection_options ||= {
           builder: middleware,
@@ -49,10 +62,18 @@ module Ably
         }
       end
 
+      # Return a Faraday middleware stack to initiate the Faraday::Connection with
+      #
+      # @see http://mislav.uniqpath.com/2011/07/faraday-advanced-http/
       def middleware
         @middleware ||= Faraday::Builder.new do |builder|
+          # Convert request params to "www-form-urlencoded"
           builder.use Faraday::Request::UrlEncoded
+
+          # Parse JSON response bodies
           builder.use Ably::Rest::Middleware::ParseJson
+
+          # Set Faraday's HTTP adapter
           builder.adapter Faraday.default_adapter
         end
       end
