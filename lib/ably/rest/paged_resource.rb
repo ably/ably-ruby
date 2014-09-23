04bde80c53228c0d228af175daa4d8294d2f5ffa
Matthew O'Riordan <matthew.oriordan@gmail.com>
2014-09-23 13:29:45 +0100

    Better documentation for PagedResource

--- lib/ably/rest/paged_resource.rb
+++ lib/ably/rest/paged_resource.rb
@@ -1,8 +1,17 @@
 module Ably
   module Rest
+    # Wraps any Ably HTTP response that supports paging and automatically provides methdos to iterated through
+    # the array of resources using {#first}, {#next}, {#last?} and {#first?}
+    #
+    # Paging information is provided by Ably in the LINK HTTP headers
     class PagedResource
       include Enumerable
 
+      # @param [Faraday::Response] http_response Initial HTTP response from an Ably request to a paged resource
+      # @param [String] base_url Base URL for request that generated the http_response so that subsequent paged requests can be made
+      # @param [Ably::Rest::Client] client {Ably::Client} used to make the request to Ably
+      #
+      # @return [Ably::Rest::PagedResource]
       def initialize(http_response, base_url, client)
         @http_response = http_response
         @body          = http_response.body
@@ -10,24 +19,67 @@ module Ably
         @base_url      = "#{base_url.gsub(%r{/[^/]*$}, '')}/"
       end
 
+      # Retrieve the first page of results
+      #
+      # @return [Ably::Rest::PagedResource]
       def first
         PagedResource.new(@client.get(pagination_url('first')), @base_url, @client)
       end
 
+      # Retrieve the next page of results
+      #
+      # @return [Ably::Rest::PagedResource]
       def next
         PagedResource.new(@client.get(pagination_url('next')), @base_url, @client)
       end
 
+      # True if this is the last page in the paged resource set
+      #
+      # @return [Boolean]
       def last?
-        !supports_paging? ||
+        !supports_pagination? ||
           pagination_header('next').nil?
       end
 
+      # True if this is the first page in the paged resource set
+      #
+      # @return [Boolean]
       def first?
-        !supports_paging? ||
+        !supports_pagination? ||
           pagination_header('first') == pagination_header('current')
       end
 
+      # True if the HTTP response supports paging with the expected LINK HTTP headers
+      #
+      # @return [Boolean]
+      def supports_pagination?
+        !pagination_headers.empty?
+      end
+
+      # Standard Array accessor method
+      def [](index)
+        @body[index]
+      end
+
+      # Returns number of items within this page, not the total number of items in the entire paged resource set
+      def length
+        @body.length
+      end
+      alias_method :count, :length
+      alias_method :size,  :length
+
+      # Method ensuring this {Ably::Rest::PagedResource} is {http://ruby-doc.org/core-2.1.3/Enumerable.html Enumerable}
+      def each(&block)
+        @body.each do |item|
+          if block_given?
+            block.call item
+          else
+            yield item
+          end
+        end
+      end
+
+      private
       def pagination_headers
         link_regex = %r{<(?<url>[^>]+)>; rel="(?<rel>[^"]+)"}
         @pagination_headers ||= @http_response.headers['link'].scan(link_regex).inject({}) do |hash, val_array|
@@ -48,34 +100,6 @@ module Ably
           "#{@base_url}#{pagination_header(id)[2..-1]}"
         else
           pagination_header[id]
-        end
-      end
-
-      def supports_paging?
-        !pagination_headers.empty?
-      end
-
-      def [](index)
-        @body[index]
-      end
-
-      def length
-        @body.length
-      end
-      alias_method :count, :length
-      alias_method :size,  :length
-
-      def count
-        @body.count
-      end
-
-      def each(&block)
-        @body.each do |item|
-          if block_given?
-            block.call item
-          else
-            yield item
-          end
         end
       end
     end
