ccb5583bb5341f5c595e0d09cf116e8e2c0ffced
Matthew O'Riordan <matthew.oriordan@gmail.com>
2014-09-24 14:27:11 +0100

    Message for Rest is different to Realtime to message so namespace accordingly
    Restructure unit tests ready for Rest vs Realtime

--- spec/unit/realtime_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-describe Ably::Realtime do
-  let(:options) { { api_key: 'app.key:secret' } }
-
-  specify 'constructor returns an Ably::Realtime::Client' do
-    expect(Ably::Realtime.new(options)).to be_instance_of(Ably::Realtime::Client)
-  end
-end
--- spec/unit/rest/message_spec.rb
+++ spec/unit/rest/message_spec.rb
@@ -1,12 +1,12 @@
 require "spec_helper"
 
-describe Ably::Message do
+describe Ably::Rest::Message do
   context 'attributes' do
     let(:unique_value) { 'unique_value' }
 
-    %w(name data client_id timestamp channel_serial).each do |attribute|
+    %w(name data client_id message_id).each do |attribute|
       context "##{attribute}" do
-        subject { Ably::Message.new({ attribute.to_sym => unique_value }) }
+        subject { Ably::Rest::Message.new({ attribute.to_sym => unique_value }) }
 
         it "retrieves attribute :#{attribute}" do
           expect(subject.public_send(attribute)).to eql(unique_value)
@@ -14,16 +14,24 @@ describe Ably::Message do
       end
     end
 
-    context '#timestamp_at' do
-      subject { Ably::Message.new(timestamp: Time.now.to_i * 1000) }
-      it 'retrieves attribute :key' do
-        expect(subject.timestamp_at.to_i).to be_within(1).of(Time.now.to_i)
+    context '#sender_timestamp_at' do
+      subject { Ably::Rest::Message.new(timestamp: Time.now.to_i * 1000) }
+      it 'retrieves attribute :timestamp' do
+        expect(subject.sender_timestamp_at.to_i).to be_within(1).of(Time.now.to_i)
+      end
+    end
+
+    context '#sender_timestamp' do
+      let(:timestamp) { Time.now.to_i * 1000 }
+      subject { Ably::Rest::Message.new(timestamp: timestamp) }
+      it 'retrieves attribute :timestamp' do
+        expect(subject.sender_timestamp).to eql(timestamp)
       end
     end
 
     context '#raw_message' do
       let(:attributes) { { timestamp: Time.now.to_i * 1000 } }
-      subject { Ably::Message.new(attributes) }
+      subject { Ably::Rest::Message.new(attributes) }
 
       it 'provides access to #raw_message' do
         expect(subject.raw_message).to eql(attributes)
@@ -31,7 +39,7 @@ describe Ably::Message do
     end
 
     context '#[]' do
-      subject { Ably::Message.new(unusual: 'attribute') }
+      subject { Ably::Rest::Message.new(unusual: 'attribute') }
 
       it 'provides accessor method to #raw_message' do
         expect(subject[:unusual]).to eql('attribute')
@@ -43,22 +51,22 @@ describe Ably::Message do
     let(:attributes) { { client_id: 'unique' } }
 
     it 'is true when attributes are the same' do
-      new_message = -> { Ably::Message.new(attributes) }
+      new_message = -> { Ably::Rest::Message.new(attributes) }
       expect(new_message[]).to eq(new_message[])
     end
 
     it 'is false when attributes are not the same' do
-      expect(Ably::Message.new(client_id: 1)).to_not eq(Ably::Message.new(client_id: 2))
+      expect(Ably::Rest::Message.new(client_id: 1)).to_not eq(Ably::Rest::Message.new(client_id: 2))
     end
 
     it 'is false when class type differs' do
-      expect(Ably::Message.new(client_id: 1)).to_not eq(nil)
+      expect(Ably::Rest::Message.new(client_id: 1)).to_not eq(nil)
     end
   end
 
   context 'is immutable' do
     let(:options) { { client_id: 'John' } }
-    subject { Ably::Message.new(options) }
+    subject { Ably::Rest::Message.new(options) }
 
     it 'prevents changes' do
       expect { subject.raw_message[:client_id] = 'Joe' }.to raise_error RuntimeError, /can't modify frozen Hash/
