d11df649c1cf038d34d5ffeb5f7732c564261471
Matthew O'Riordan <matthew.oriordan@gmail.com>
2014-10-30 14:23:05 +0000

    Never update connection#message_serial from received messages

--- lib/ably/realtime/client/incoming_message_dispatcher.rb
+++ lib/ably/realtime/client/incoming_message_dispatcher.rb
@@ -40,7 +40,7 @@ module Ably::Realtime
           logger.debug "#{protocol_message.action} received: #{protocol_message}"
         end
 
-        update_connection_serial protocol_message
+        update_connection_id protocol_message
 
         case protocol_message.action
           when ACTION.Heartbeat
@@ -86,11 +86,11 @@ module Ably::Realtime
         end
       end
 
-      def update_connection_serial(protocol_message)
+      def update_connection_id(protocol_message)
         if protocol_message.connection_id && (protocol_message.connection_id != connection.id)
           logger.debug "New connection ID set to #{protocol_message.connection_id}"
+          connection.update_connection_id protocol_message.connection_id
         end
-        connection.update_connection_serial protocol_message.connection_id, protocol_message.connection_serial
       end
 
       def ack_pending_queue_for_message_serial(ack_protocol_message)
