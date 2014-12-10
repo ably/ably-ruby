d6472ee4e1dd264f14f954812c69621da498dde4
Matthew O'Riordan <matthew.oriordan@gmail.com>
2014-12-10 21:52:36 +0000

    Show MsgPack packed request body as Json in logs

--- lib/ably/rest/middleware/logger.rb
+++ lib/ably/rest/middleware/logger.rb
@@ -18,18 +18,31 @@ module Ably
 
         def call(env)
           debug "=> URL: #{env.method} #{env.url}, Headers: #{dump_headers env.request_headers}"
-          debug "=> Body: #{env.body}"
+          debug ">= Body: #{body_for(env)}"
           @app.call env
         end
 
         def on_complete(env)
           debug "<= Status: #{env.status}, Headers: #{dump_headers env.response_headers}"
-          debug "<= Body: #{env.body}"
+          debug "<= Body: #{body_for(env)}"
         end
 
         private
         def dump_headers(headers)
           headers.map { |k, v| "#{k}: #{v.inspect}" }.join(", ")
+        end
+
+        def body_for(env)
+          return '' if !env.body || env.body.empty?
+
+          if env.request_headers['Content-Type'] == 'application/x-msgpack'
+            MessagePack.unpack(env.body)
+          else
+            env.body
+          end
+
+        rescue StandardError
+          "Error displaying body: (as hex) '#{env.body.unpack('H*')}'"
         end
       end
     end
