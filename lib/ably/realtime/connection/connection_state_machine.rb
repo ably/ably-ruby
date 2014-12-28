b4789813596fd423467981311710916119c407e0
Matthew O'Riordan <matthew.oriordan@gmail.com>
2014-12-28 10:50:37 +0200

    Force close connection if CLOSED message not received from server

--- lib/ably/realtime/connection/connection_state_machine.rb
+++ lib/ably/realtime/connection/connection_state_machine.rb
@@ -2,39 +2,10 @@ require 'statesman'
 
 module Ably::Realtime
   class Connection
-    module StatesmanMonkeyPatch
-      # Override Statesman's #before_transition to support :from arrays
-      # This can be removed once https://github.com/gocardless/statesman/issues/95 is solved
-      def before_transition(options = nil, &block)
-        arrayify_transition(options) do |options_without_from_array|
-          super *options_without_from_array, &block
-        end
-      end
-
-      def after_transition(options = nil, &block)
-        arrayify_transition(options) do |options_without_from_array|
-          super *options_without_from_array, &block
-        end
-      end
-
-      private
-      def arrayify_transition(options, &block)
-        if options.nil?
-          yield []
-        elsif options.fetch(:from, nil).kind_of?(Array)
-          options[:from].each do |from_state|
-            yield [options.merge(from: from_state)]
-          end
-        else
-          yield [options]
-        end
-      end
-    end
-
     # Internal class to manage connection state, recovery and state transitions for an {Ably::Realtime::Connection}
     class ConnectionStateMachine
       include Statesman::Machine
-      extend StatesmanMonkeyPatch
+      extend Ably::Modules::StatesmanMonkeyPatch
 
       # States supported by this StateMachine match #{Connection::STATE}s
       #   :initialized
@@ -98,6 +69,8 @@ module Ably::Realtime
       # Override Statesman's #transition_to so that:
       # * log state change failures to {Logger}
       # * raise an exception on the {Ably::Realtime::Connection}
+      #
+      # @return [void]
       def transition_to(state, *args)
         unless result = super(state, *args)
           error_message = "ConnectionStateMachine: Unable to transition from #{current_state} => #{state}"
@@ -107,31 +80,17 @@ module Ably::Realtime
         result
       end
 
+      # @return [Statesman History Object]
       def previous_transition
         history[-2]
       end
 
+      # @return [Symbol]
       def previous_state
         previous_transition.to_state if previous_transition
       end
 
       private
-      # TODO: Implement once CLOSED ProtocolMessage is sent back from Ably in response to a CLOSE message
-      #
-      # FORCE_CONNECTION_CLOSED_TIMEOUT = 5
-      #
-      # def force_closed_unless_server_acknowledge_closed
-      #   timeouts[:close_connection] << EventMachine::Timer.new(FORCE_CONNECTION_CLOSED_TIMEOUT) do
-      #     transition_to :closed
-      #   end
-      # end
-      #
-      # def clear_force_closed_timeouts
-      #   timeouts[:close_connection].each do |timeout|
-      #     timeout.cancel
-      #   end.clear
-      # end
-
       def connection
         object
       end
