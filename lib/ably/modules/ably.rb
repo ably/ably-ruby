983f8c8c17f8798d87b24bf877167a8df7a9a1a0
Matthew O'Riordan <matthew.oriordan@gmail.com>
2015-01-03 23:43:16 +0200

    Use fallback hosts for REST requests that fail on primary domain

--- lib/ably/modules/ably.rb
+++ lib/ably/modules/ably.rb
@@ -5,7 +5,7 @@
 # @see file:README.md README
 module Ably
   # Fallback hosts to use when a connection to rest/realtime.ably.io is not possible due to
-  #   network failures either at the client, between the client and Ably, within an Ably data center, or at the IO domain registrar
+  # network failures either at the client, between the client and Ably, within an Ably data center, or at the IO domain registrar
   #
   FALLBACK_HOSTS = %w(A.ably-realtime.com B.ably-realtime.com C.ably-realtime.com D.ably-realtime.com E.ably-realtime.com)
 end
