a121ce9921bdc342531e2a88dbb73064f6c63557
Matthew O'Riordan <matthew.oriordan@gmail.com>
2015-01-08 15:45:50 +0000

    Global #console method that can be used when testing to debut objects or messages, only available in test environment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
+# Output the message to the console
+# Useful for debugging as clearly visible, and name is not used anywhere else in library as opposed to debug or puts
+def console(message)
+  puts "\033[31m[#{Time.now.strftime('%H:%M:%S.%L')}]\033[0m \033[33m#{message}\033[0m"
+end
+
 require 'webmock/rspec'
 
 require 'ably'
