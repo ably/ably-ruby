8b14cd9ef4f8a301df87779c71ee936d594f52dd
Matthew O'Riordan <matthew.oriordan@gmail.com>
2015-01-11 14:12:54 +0000

    Refactor final realtime tests to use EventMachine helpers

--- spec/acceptance/realtime/stats_spec.rb
+++ spec/acceptance/realtime/stats_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 
-describe Ably::Realtime::Client, '#stats' do
-  include RSpec::EventMachine
-
+describe Ably::Realtime::Client, '#stats', :event_machine do
   vary_by_protocol do
     let(:client) do
       Ably::Realtime::Client.new(api_key: api_key, environment: environment, protocol: protocol)
@@ -10,19 +8,15 @@ describe Ably::Realtime::Client, '#stats' do
 
     describe 'fetching stats' do
       it 'should return a Hash' do
-        run_reactor do
-          client.stats do |stats|
-            expect(stats).to be_a(Array)
-            stop_reactor
-          end
+        client.stats do |stats|
+          expect(stats).to be_a(Array)
+          stop_reactor
         end
       end
 
-      it 'should return a deferrable object' do
-        run_reactor do
-          expect(client.stats).to be_a(EventMachine::Deferrable)
-          stop_reactor
-        end
+      it 'should return a Deferrable object' do
+        expect(client.stats).to be_a(EventMachine::Deferrable)
+        stop_reactor
       end
     end
   end
