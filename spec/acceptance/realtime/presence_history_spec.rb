58c73fdb5b0778d959233ba1aa146b62b64ee1b3
Matthew O'Riordan <matthew.oriordan@gmail.com>
2015-01-11 14:12:54 +0000

    Refactor final realtime tests to use EventMachine helpers

--- spec/acceptance/realtime/presence_history_spec.rb
+++ spec/acceptance/realtime/presence_history_spec.rb
@@ -1,9 +1,7 @@
 # encoding: utf-8
 require 'spec_helper'
 
-describe Ably::Realtime::Presence, 'history' do
-  include RSpec::EventMachine
-
+describe Ably::Realtime::Presence, 'history', :event_machine do
   vary_by_protocol do
     let(:default_options)     { { api_key: api_key, environment: environment, protocol: protocol } }
 
@@ -20,41 +18,37 @@ describe Ably::Realtime::Presence, 'history' do
     let(:data)                { random_str }
 
     it 'provides up to the moment presence history' do
-      run_reactor do
-        presence_client_one.enter(data: data) do
-          presence_client_one.leave do
-            presence_client_one.history do |history|
-              expect(history.count).to eql(2)
+      presence_client_one.enter(data: data) do
+        presence_client_one.leave do
+          presence_client_one.history do |history|
+            expect(history.count).to eql(2)
 
-              expect(history[1].action).to eq(:enter)
-              expect(history[1].client_id).to eq(client_one.client_id)
-              expect(history[1].data).to eql(data)
+            expect(history[1].action).to eq(:enter)
+            expect(history[1].client_id).to eq(client_one.client_id)
+            expect(history[1].data).to eql(data)
 
-              expect(history[0].action).to eq(:leave)
-              expect(history[0].client_id).to eq(client_one.client_id)
-              expect(history[0].data).to be_nil
+            expect(history[0].action).to eq(:leave)
+            expect(history[0].client_id).to eq(client_one.client_id)
+            expect(history[0].data).to be_nil
 
-              stop_reactor
-            end
+            stop_reactor
           end
         end
       end
     end
 
     it 'ensures REST presence history message IDs match ProtocolMessage wrapped message and member IDs via Realtime' do
-      run_reactor do
-        presence_client_one.subscribe(:enter) do |message|
-          presence_client_one.history do |history|
-            expect(history.count).to eql(1)
+      presence_client_one.subscribe(:enter) do |message|
+        presence_client_one.history do |history|
+          expect(history.count).to eql(1)
 
-            expect(history[0].id).to eql(message.id)
-            expect(history[0].member_id).to eql(message.member_id)
-            stop_reactor
-          end
+          expect(history[0].id).to eql(message.id)
+          expect(history[0].member_id).to eql(message.member_id)
+          stop_reactor
         end
-
-        presence_client_one.enter(data: data)
       end
+
+      presence_client_one.enter(data: data)
     end
   end
 end
