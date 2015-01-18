e893571587652c2cf66c3b6f7b133139824f8640
Matthew O'Riordan <matthew.oriordan@gmail.com>
2015-01-18 12:00:54 +0000

    Reload the TestApp credentials when a test fails to ensure state is not shared

---
+++ spec/support/rest_testapp_before_retry.rb
@@ -0,0 +1,15 @@
+# If a test fails and RSPEC_RETRY is set to true, create a new
+# application before retrying the RSpec test again
+#
+RSpec.configure do |config|
+  config.around(:example) do |example|
+    example.run
+
+    next if example.metadata[:webmock] # new app is not needed for a mocked test
+
+    if example.exception && ENV['RSPEC_RETRY']
+      TestApp.reload
+      puts "** Test app reloaded before next retry **"
+    end
+  end
+end
