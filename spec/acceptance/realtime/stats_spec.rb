bf0aff4942c905a6f76a242388bdae8f31bef56b
Matthew O'Riordan <matthew.oriordan@gmail.com>
2015-08-17 18:13:15 +0100

    Coverage for requirement RTC5b
    See http://docs.ably.io/client-lib-development-guide/features/

--- spec/acceptance/realtime/stats_spec.rb
+++ spec/acceptance/realtime/stats_spec.rb
@@ -7,10 +7,22 @@ describe Ably::Realtime::Client, '#stats', :event_machine do
     end
 
     describe 'fetching stats' do
-      it 'should return a PaginatedResult' do
+      it 'returns a PaginatedResult' do
         client.stats do |stats|
           expect(stats).to be_a(Ably::Models::PaginatedResult)
           stop_reactor
+        end
+      end
+
+      context 'with options' do
+        let(:options) { { arbitrary: random_str } }
+
+        it 'passes the option arguments to the REST stat method' do
+          expect(client.rest_client).to receive(:stats).with(options)
+
+          client.stats(options) do |stats|
+            stop_reactor
+          end
         end
       end
 
