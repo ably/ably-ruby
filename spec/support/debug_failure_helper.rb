ffaa93ba9f5837ab6bf43c4865c1014a84dd443c
Matthew O'Riordan <matthew.oriordan@gmail.com>
2015-11-07 20:10:07 +0000

    Include timestamp in CI failure debug logs

--- spec/support/debug_failure_helper.rb
+++ spec/support/debug_failure_helper.rb
@@ -3,7 +3,7 @@ RSpec.configure do |config|
     @log_output = []
     %w(fatal error warn info debug).each do |method_name|
       allow_any_instance_of(Ably::Logger).to receive(method_name.to_sym).and_wrap_original do |method, *args|
-        @log_output << "[#{method_name}] #{args[0]}"
+        @log_output << "#{Time.now.strftime('%H:%M:%S.%L')} [\e[33m#{method_name}\e[0m] #{args[0]}"
         method.call(*args)
       end
     end
@@ -11,6 +11,6 @@ RSpec.configure do |config|
 
   config.after(:example) do |example|
     exception = example.exception
-    puts "\n#{'-'*35}\nVerbose Ably log from test failure:\n#{'-'*35}\n#{@log_output.join("\n")}\n\n" if exception
+    puts "\n#{'-'*34}\n\e[36mVerbose Ably log from test failure\e[0m\n#{'-'*34}\n#{@log_output.join("\n")}\n\n" if exception
   end
 end
