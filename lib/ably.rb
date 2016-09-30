b8f8f7084cf549ef99ecb10a6e6bb88f9c437bd0
Matthew O'Riordan <matthew.oriordan@gmail.com>
2016-10-01 00:28:55 +0100

    Move lib_variant option into share Version module
    This is used by ably-ruby-rest yet was not included as lib/ably.rb is realtime specific.

--- lib/ably.rb
+++ lib/ably.rb
@@ -1,5 +1,7 @@
 require 'addressable/uri'
 
+require 'ably/version'
+
 %w(modules util).each do |namespace|
   Dir.glob(File.expand_path("ably/#{namespace}/*.rb", File.dirname(__FILE__))).sort.each do |file|
     require file
@@ -11,17 +13,3 @@ require 'ably/exceptions'
 require 'ably/logger'
 require 'ably/realtime'
 require 'ably/rest'
-require 'ably/version'
-
-# Allow a variant to be configured for all instances of this client library
-# such as ruby-rest-[VERSION]
-module Ably
-  # @api private
-  def self.lib_variant=(variant)
-    @lib_variant = variant
-  end
-
-  def self.lib_variant
-    @lib_variant
-  end
-end
--- lib/ably/version.rb
+++ lib/ably/version.rb
@@ -1,4 +1,16 @@
 module Ably
   VERSION = '0.8.13'
   PROTOCOL_VERSION = '0.8'
+
+  # Allow a variant to be configured for all instances of this client library
+  # such as ruby-rest-[VERSION]
+
+  # @api private
+  def self.lib_variant=(variant)
+    @lib_variant = variant
+  end
+
+  def self.lib_variant
+    @lib_variant
+  end
 end
