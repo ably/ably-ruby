d487acf4a7eef1cd1dbd61395e7a3a1c84f8e300
Matthew O'Riordan <matthew.oriordan@gmail.com>
2017-04-11 01:05:30 +0100

    Add push notification device models

---
+++ spec/unit/models/device_push_details_spec.rb
@@ -0,0 +1,100 @@
+# encoding: utf-8
+require 'spec_helper'
+require 'shared/model_behaviour'
+
+describe Ably::Models::DevicePushDetails do
+  include Ably::Modules::Conversions
+
+  subject { Ably::Models::DevicePushDetails }
+
+  %w(transport_id transport_type transport_token state).each do |string_attribute|
+    let(:empty_push_details) { subject.new }
+
+    describe "##{string_attribute} and ##{string_attribute}=" do
+      let(:new_val) { random_str }
+
+      specify 'setter accepts a string value and getter returns the new value' do
+        expect(empty_push_details.public_send(string_attribute)).to be_nil
+        empty_push_details.public_send("#{string_attribute}=", new_val)
+        expect(empty_push_details.public_send(string_attribute)).to eql(new_val)
+      end
+
+      specify 'setter accepts nil' do
+        empty_push_details.public_send("#{string_attribute}=", new_val)
+        expect(empty_push_details.public_send(string_attribute)).to eql(new_val)
+        empty_push_details.public_send("#{string_attribute}=", nil)
+        expect(empty_push_details.public_send(string_attribute)).to be_nil
+      end
+
+      specify 'rejects non string or nil values' do
+        expect { empty_push_details.public_send("#{string_attribute}=", {}) }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  context 'camelCase constructor attributes' do
+    let(:transport_id) { random_str }
+    let(:push_details) { subject.new("transportId" => transport_id ) }
+
+    specify 'are rubyfied and exposed as underscore case' do
+      expect(push_details.transport_id).to eql(transport_id)
+    end
+
+    specify 'are generated when the object is serialised to JSON' do
+      expect(JSON.parse(push_details.to_json)["transportId"]).to eql(transport_id)
+    end
+  end
+
+  describe "#metadata and #metadata=" do
+    let(:new_val) { { foo: random_str } }
+
+    specify 'setter accepts a Hash value and getter returns the new value' do
+      expect(empty_push_details.metadata).to eql({})
+      empty_push_details.metadata = new_val
+      expect(empty_push_details.metadata.to_json).to eql(new_val.to_json)
+    end
+
+    specify 'setter accepts nil but always returns an empty hash' do
+      empty_push_details.metadata = new_val
+      expect(empty_push_details.metadata.to_json).to eql(new_val.to_json)
+      empty_push_details.metadata = nil
+      expect(empty_push_details.metadata).to eql({})
+    end
+
+    specify 'rejects non Hash or nil values' do
+      expect { empty_push_details.metadata = "foo" }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#error_reason and #error_reason=" do
+    let(:error_message) { random_str }
+    let(:error_attributes) { { message: error_message } }
+
+    specify 'setter accepts a ErrorInfo object and getter returns a ErrorInfo object' do
+      expect(empty_push_details.error_reason).to be_nil
+      empty_push_details.error_reason = ErrorInfo(error_attributes)
+      expect(empty_push_details.error_reason).to be_a(Ably::Models::ErrorInfo)
+      expect(empty_push_details.error_reason.message).to eql(error_message)
+      expect(empty_push_details.error_reason.to_json).to eql(error_attributes.to_json)
+    end
+
+    specify 'setter accepts a Hash value and getter returns a ErrorInfo object' do
+      expect(empty_push_details.error_reason).to be_nil
+      empty_push_details.error_reason = error_attributes
+      expect(empty_push_details.error_reason).to be_a(Ably::Models::ErrorInfo)
+      expect(empty_push_details.error_reason.message).to eql(error_message)
+      expect(empty_push_details.error_reason.to_json).to eql(error_attributes.to_json)
+    end
+
+    specify 'setter accepts nil values' do
+      empty_push_details.error_reason = error_attributes
+      expect(empty_push_details.error_reason.to_json).to eql(error_attributes.to_json)
+      empty_push_details.error_reason = nil
+      expect(empty_push_details.error_reason).to be_nil
+    end
+
+    specify 'rejects non Hash, ErrorInfo or nil values' do
+      expect { empty_push_details.metadata = "foo" }.to raise_error(ArgumentError)
+    end
+  end
+end
