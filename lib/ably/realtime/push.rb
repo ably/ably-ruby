ee8a66a6988fd2507a7376ce088961e85245891a
Matthew O'Riordan <matthew.oriordan@gmail.com>
2017-04-25 15:42:14 +0100

    push: Move direct publish into push.admin

--- lib/ably/realtime/push.rb
+++ lib/ably/realtime/push.rb
@@ -4,29 +4,11 @@ module Ably
   module Realtime
     # Class providing push notification functionality
     class Push
-      include Ably::Modules::AsyncWrapper
-
       # @private
       attr_reader :client
 
       def initialize(client)
         @client = client
-      end
-
-      # (see Ably::Rest::Push#publish)
-      #
-      # @yield  Block is invoked upon successful publish of the message
-      # @return [Ably::Util::SafeDeferrable]
-      #
-      def publish(recipient, data, &callback)
-        raise ArgumentError, "Expecting a Hash object for recipient, got #{recipient.class}" unless recipient.kind_of?(Hash)
-        raise ArgumentError, "Recipient data is empty. You must provide recipient details" if recipient.empty?
-        raise ArgumentError, "Expecting a Hash object for data, got #{data.class}" unless data.kind_of?(Hash)
-        raise ArgumentError, "Push data field is empty. You must provide attributes for the push notification" if data.empty?
-
-        async_wrap(callback) do
-          rest_push.publish(recipient, data)
-        end
       end
 
       # Admin features for push notifications like managing devices and channel subscriptions
@@ -52,14 +34,6 @@ module Ably
       private
       def raise_unsupported
         raise Ably::Exceptions::PushNotificationsNotSupported, 'This device does not support receiving or subscribing to push notifications. All PushChannel methods are unavailable'
-      end
-
-      def rest_push
-        client.rest_client.push
-      end
-
-      def logger
-        client.logger
       end
     end
   end
--- lib/ably/realtime/push/admin.rb
+++ lib/ably/realtime/push/admin.rb
@@ -6,6 +6,7 @@ module Ably::Realtime
     # Class providing push notification administrative functionality
     # for registering devices and attaching to channels etc.
     class Admin
+      include Ably::Modules::AsyncWrapper
       include Ably::Modules::Conversions
 
       # @api private
@@ -19,6 +20,22 @@ module Ably::Realtime
         @client = push.client
       end
 
+      # (see Ably::Rest::Push#publish)
+      #
+      # @yield  Block is invoked upon successful publish of the message
+      # @return [Ably::Util::SafeDeferrable]
+      #
+      def publish(recipient, data, &callback)
+        raise ArgumentError, "Expecting a Hash object for recipient, got #{recipient.class}" unless recipient.kind_of?(Hash)
+        raise ArgumentError, "Recipient data is empty. You must provide recipient details" if recipient.empty?
+        raise ArgumentError, "Expecting a Hash object for data, got #{data.class}" unless data.kind_of?(Hash)
+        raise ArgumentError, "Push data field is empty. You must provide attributes for the push notification" if data.empty?
+
+        async_wrap(callback) do
+          rest_push_admin.publish(recipient, data)
+        end
+      end
+
       # Manage device registrations
       # @return [Ably::Realtime::Push::DeviceRegistrations]
       def device_registrations
@@ -29,6 +46,15 @@ module Ably::Realtime
       # @return [Ably::Realtime::Push::ChannelSubscriptions]
       def channel_subscriptions
         @channel_subscriptions ||= ChannelSubscriptions.new(self)
+      end
+
+      private
+      def rest_push_admin
+        client.rest_client.push.admin
+      end
+
+      def logger
+        client.logger
       end
     end
   end
