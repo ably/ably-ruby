16256ea2f4625d0f5dfb9f51a155a059234dfb10
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-04-25 19:40:01 +0200

    Removed ruby 1.9.3 from supported versions. Update outdated gems.

--- spec/acceptance/rest/client_spec.rb
+++ spec/acceptance/rest/client_spec.rb
@@ -1081,7 +1081,7 @@ describe Ably::Rest::Client do
     end
 
     context 'version headers', :webmock do
-      [nil, 'ably-ruby/1.1.1 ruby/1.9.3'].each do |agent|
+      [nil, 'ably-ruby/1.1.1 ruby/3.1.1'].each do |agent|
         context "with #{agent ? "custom #{agent}" : 'default'} agent" do
           let(:client_options) { default_options.merge(key: api_key, agent: agent) }
 
