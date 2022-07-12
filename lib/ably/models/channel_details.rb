d3092661be995605c7ff675f7f414ee037988707
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:33:27 +0200

    Fix lib/ably/models/channel_details.rb

--- lib/ably/models/channel_details.rb
+++ lib/ably/models/channel_details.rb
@@ -1,59 +1,65 @@
-module Ably::Models
-  # Convert token details argument to a {ChannelDetails} object
+# frozen_string_literal: true
+
+module Ably
+  # Models module provides the methods and classes for the Ably library
   #
-  # @param attributes (see #initialize)
-  #
-  # @return [ChannelDetails]
-  def self.ChannelDetails(attributes)
-    case attributes
-    when ChannelDetails
-      return attributes
-    else
-      ChannelDetails.new(attributes)
-    end
-  end
-
-  # ChannelDetails is a type that represents information for a channel including channelId, name, status and occupancy (CHD1)
-  #
-  class ChannelDetails
-    extend Ably::Modules::Enum
-    extend Forwardable
-    include Ably::Modules::ModelCommon
-
-    # The attributes of ChannelDetails (CHD2)
+  module Models
+    # Convert token details argument to a {ChannelDetails} object
     #
-    attr_reader :attributes
-
-    alias_method :to_h, :attributes
-
-    # Initialize a new ChannelDetails
+    # @param attributes (see #initialize)
     #
-    def initialize(attrs)
-      @attributes = IdiomaticRubyWrapper(attrs.clone)
+    # @return [ChannelDetails]
+    def self.ChannelDetails(attributes)
+      case attributes
+      when ChannelDetails
+        attributes
+      else
+        ChannelDetails.new(attributes)
+      end
     end
 
-    # The identifier of the channel (CHD2a)
+    # ChannelDetails is a type that represents information for a channel including channelId, name, status and occupancy (CHD1)
     #
-    # @return [String]
-    #
-    def channel_id
-      attributes[:channel_id]
-    end
+    class ChannelDetails
+      extend ::Ably::Modules::Enum
+      extend Forwardable
+      include ::Ably::Modules::ModelCommon
 
-    # The identifier of the channel (CHD2a)
-    #
-    # @return [String]
-    #
-    def name
-      attributes[:name]
-    end
+      # The attributes of ChannelDetails (CHD2)
+      #
+      attr_reader :attributes
 
-    # The status of the channel (CHD2b)
-    #
-    # @return [Ably::Models::ChannelStatus, nil]
-    #
-    def status
-      Ably::Models::ChannelStatus(attributes[:status])
+      alias_method :to_h, :attributes
+
+      # Initialize a new ChannelDetails
+      #
+      def initialize(attrs)
+        @attributes = IdiomaticRubyWrapper(attrs.clone)
+      end
+
+      # The identifier of the channel (CHD2a)
+      #
+      # @return [String]
+      #
+      def channel_id
+        attributes[:channel_id]
+      end
+
+      # The identifier of the channel (CHD2a)
+      #
+      # @return [String]
+      #
+      def name
+        attributes[:name]
+      end
+
+      # The status of the channel (CHD2b)
+      #
+      # @return [Ably::Models::ChannelStatus, nil]
+      #
+      def status
+        Ably::Models::ChannelStatus(attributes[:status])
+      end
     end
   end
 end
