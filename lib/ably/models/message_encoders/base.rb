f700c4dce5142d309701a9e9d1804b432e153bf8
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:29:44 +0200

    Fix lib/ably/models/message_encoders/base.rb

--- lib/ably/models/message_encoders/base.rb
+++ lib/ably/models/message_encoders/base.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'ably/modules/conversions'
 
 # MessageEncoders are registered with the Ably client library and are responsible
@@ -9,8 +11,10 @@ require 'ably/modules/conversions'
 #
 module Ably
   module Models
+    # MessageEncoders module provides the methods and classes for the Ably library
+    #
     module MessageEncoders
-      extend Ably::Modules::Conversions
+      extend ::Ably::Modules::Conversions
 
       # Base interface for an Ably Encoder
       #
@@ -32,8 +36,8 @@ module Ably
         # @param [Hash] channel_options  the options used to initialize the channel that this message was received on
         #
         # @return [void]
-        def encode(message, channel_options)
-          raise "Not yet implemented"
+        def encode(_message, _channel_options)
+          raise 'Not yet implemented'
         end
 
         # #decode is called once for every encoding step
@@ -48,8 +52,8 @@ module Ably
         # @param [Hash] channel_options  the options used to initialize the channel that this message was received on
         #
         # @return [void]
-        def decode(message, channel_options)
-          raise "Not yet implemented"
+        def decode(_message, _channel_options)
+          raise 'Not yet implemented'
         end
 
         # Add encoding to the message Hash.
@@ -68,9 +72,9 @@ module Ably
         #
         # @return [String,nil]
         def current_encoding_part(message)
-          if message[:encoding]
-            message[:encoding].split('/')[-1]
-          end
+          return unless message[:encoding]
+
+          message[:encoding].split('/')[-1]
         end
 
         # Strip the current encoding part within the message Hash.
@@ -82,7 +86,8 @@ module Ably
         #
         # @return [void]
         def strip_current_encoding_part(message)
-          raise "Cannot strip encoding when there is no encoding for this message" unless message[:encoding]
+          raise 'Cannot strip encoding when there is no encoding for this message' unless message[:encoding]
+
           message[:encoding] = message[:encoding].split('/')[0...-1].join('/')
           message[:encoding] = nil if message[:encoding].empty?
         end
@@ -100,23 +105,24 @@ module Ably
       # @api private
       def self.register_default_encoders(client, options = {})
         binary_protocol = !!options[:binary_protocol]
-        client.register_encoder Ably::Models::MessageEncoders::Utf8
-        client.register_encoder Ably::Models::MessageEncoders::Json
-        client.register_encoder Ably::Models::MessageEncoders::Cipher
-        client.register_encoder Ably::Models::MessageEncoders::Base64, binary_protocol: binary_protocol
+        client.register_encoder ::Ably::Models::MessageEncoders::Utf8
+        client.register_encoder ::Ably::Models::MessageEncoders::Json
+        client.register_encoder ::Ably::Models::MessageEncoders::Cipher
+        client.register_encoder ::Ably::Models::MessageEncoders::Base64, binary_protocol: binary_protocol
       end
 
       # @api private
       def self.encoder_from(encoder, options)
-        encoder_klass = if encoder.kind_of?(String)
-          encoder.split('::').inject(Kernel) do |base, klass_name|
-            base.public_send(:const_get, klass_name)
-          end
-        else
-          encoder
-        end
+        encoder_klass = if encoder.is_a?(String)
+                          encoder.split('::').inject(Kernel) do |base, klass_name|
+                            base.public_send(:const_get, klass_name)
+                          end
+                        else
+                          encoder
+                        end
 
-        raise "Encoder must inherit from `Ably::Models::MessageEncoders::Base`" unless encoder_klass.ancestors.include?(Ably::Models::MessageEncoders::Base)
+        raise 'Encoder must inherit from `Ably::Models::MessageEncoders::Base`' unless encoder_klass.ancestors.include?(::Ably::Models::MessageEncoders::Base)
+
         encoder_klass.new(self, options)
       end
     end
