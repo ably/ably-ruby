ac4bf2f1b7852c75bf4265b32f9e962001d4c3be
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:25:01 +0200

    Fix lib/ably/modules/model_common.rb

--- lib/ably/modules/model_common.rb
+++ lib/ably/modules/model_common.rb
@@ -1,77 +1,84 @@
+# frozen_string_literal: true
+
 require 'base64'
 require 'ably/modules/conversions'
 require 'ably/modules/message_pack'
 
-module Ably::Modules
-  # Common model functionality shared across many {Ably::Models}
-  module ModelCommon
-    include Conversions
-    include MessagePack
+module Ably
+  module Modules
+    # Common model functionality shared across many {Ably::Models}
+    module ModelCommon
+      include Conversions
+      include MessagePack
 
-    def self.included(base)
-      base.extend(ClassMethods)
-    end
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
 
-    # Provide a normal Hash accessor to the underlying raw message object
-    #
-    # @return [Object]
-    def [](key)
-      attributes[key]
-    end
+      # Provide a normal Hash accessor to the underlying raw message object
+      #
+      # @return [Object]
+      def [](key)
+        attributes[key]
+      end
 
-    def ==(other)
-      other.kind_of?(self.class) &&
-        attributes == other.attributes
-    end
+      def ==(other)
+        other.is_a?(self.class) &&
+          attributes == other.attributes
+      end
 
-    # Return a JSON ready object from the underlying #attributes using Ably naming conventions for keys
-    # @return [Hash]
-    def as_json(*args)
-      attributes.as_json.reject { |key, val| val.nil? }
-    end
+      # Return a JSON ready object from the underlying #attributes using Ably naming conventions for keys
+      # @return [Hash]
+      def as_json(*_arg)
+        attributes.as_json.reject { |_, val| val.nil? }
+      end
 
-    # Stringify the JSON representation of this object from the underlying #attributes
-    # @return [String]
-    def to_json(*args)
-      as_json.to_json(*args)
-    end
+      # Stringify the JSON representation of this object from the underlying #attributes
+      # @return [String]
+      def to_json(*args)
+        as_json.to_json(*args)
+      end
 
-    # @!attribute [r] hash
-    # @return [Integer] Compute a hash-code for this hash. Two hashes with the same content will have the same hash code
-    def hash
-      attributes.hash
-    end
+      # @!attribute [r] hash
+      # @return [Integer] Compute a hash-code for this hash. Two hashes with the same content will have the same hash code
+      def hash
+        attributes.hash
+      end
 
-    def to_s
-      representation = attributes.map do |key, val|
-        if val.nil?
-          nil
-        else
-          val_str = val.to_s
-          val_str = "#{val_str[0...80]}..." if val_str.length > 80
-          "#{key}=#{val_str}"
+      def to_s
+        representation = attributes.map do |key, val|
+          if val.nil?
+            nil
+          else
+            val_str = val.to_s
+            val_str = "#{val_str[0...80]}..." if val_str.length > 80
+            "#{key}=#{val_str}"
+          end
+        end
+        "<#{self.class.name}: #{representation.compact.join(', ')}>"
+      end
+
+      # ClassMethods provides the methods for the Ably library classes
+      #
+      module ClassMethods
+        # Return a new instance of this object using the provided JSON-like object or JSON string
+        # @param json_like_object  [Hash, String]  JSON-like object or JSON string
+        # @return a new instance to this object
+        def from_json(json_like_object)
+          if json_like_object.is_a?(String)
+            new(JSON.parse(json_like_object))
+          else
+            new(json_like_object)
+          end
         end
       end
-      "<#{self.class.name}: #{representation.compact.join(', ')}>"
-    end
 
-    module ClassMethods
-      # Return a new instance of this object using the provided JSON-like object or JSON string
-      # @param json_like_object  [Hash, String]  JSON-like object or JSON string
-      # @return a new instance to this object
-      def from_json(json_like_object)
-        if json_like_object.kind_of?(String)
-          new(JSON.parse(json_like_object))
-        else
-          new(json_like_object)
-        end
-      end
-    end
+      private
 
-    private
-    def ensure_utf8_string_for(attribute, value)
-      if value
-        raise ArgumentError, "#{attribute} must be a String" unless value.kind_of?(String)
+      def ensure_utf8_string_for(attribute, value)
+        return unless value
+
+        raise ArgumentError, "#{attribute} must be a String" unless value.is_a?(String)
         raise ArgumentError, "#{attribute} cannot use ASCII_8BIT encoding, please use UTF_8 encoding" unless value.encoding == Encoding::UTF_8
       end
     end
