8318d12a6d3a96430fc9baa83625f01fd84525a0
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:25:29 +0200

    Fix lib/ably/modules/http_helpers.rb

--- lib/ably/modules/http_helpers.rb
+++ lib/ably/modules/http_helpers.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'base64'
 
 require 'ably/version'
@@ -9,33 +11,34 @@ require 'ably/rest/middleware/logger'
 require 'ably/rest/middleware/parse_json'
 require 'ably/rest/middleware/parse_message_pack'
 
-module Ably::Modules
-  # HttpHelpers provides common private methods to classes to simplify HTTP interactions with Ably
-  module HttpHelpers
-    protected
-    def encode64(text)
-      Base64.encode64(text).gsub("\n", '')
-    end
+module Ably
+  module Modules
+    # HttpHelpers provides common private methods to classes to simplify HTTP interactions with Ably
+    module HttpHelpers
+      protected
 
-    def user_agent
-      "Ably Ruby client #{Ably::VERSION} (https://www.ably.io)"
-    end
-
-    def setup_outgoing_middleware(builder)
-      # Convert request params to "www-form-urlencoded"
-      builder.use Ably::Rest::Middleware::Encoder
-    end
-
-    def setup_incoming_middleware(builder, logger, options = {})
-      builder.use Ably::Rest::Middleware::Logger, logger
-
-      # Parse JSON / MsgPack response bodies. ParseJson must be first (default) parsing middleware
-      if options[:fail_if_unsupported_mime_type] == true
-        builder.use Ably::Rest::Middleware::FailIfUnsupportedMimeType
+      def encode64(text)
+        Base64.encode64(text).gsub("\n", '')
       end
 
-      builder.use Ably::Rest::Middleware::ParseJson
-      builder.use Ably::Rest::Middleware::ParseMessagePack
+      def user_agent
+        "Ably Ruby client #{Ably::VERSION} (https://www.ably.io)"
+      end
+
+      def setup_outgoing_middleware(builder)
+        # Convert request params to "www-form-urlencoded"
+        builder.use Ably::Rest::Middleware::Encoder
+      end
+
+      def setup_incoming_middleware(builder, logger, options = {})
+        builder.use Ably::Rest::Middleware::Logger, logger
+
+        # Parse JSON / MsgPack response bodies. ParseJson must be first (default) parsing middleware
+        builder.use Ably::Rest::Middleware::FailIfUnsupportedMimeType if options[:fail_if_unsupported_mime_type] == true
+
+        builder.use Ably::Rest::Middleware::ParseJson
+        builder.use Ably::Rest::Middleware::ParseMessagePack
+      end
     end
   end
 end
