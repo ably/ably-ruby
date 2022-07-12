f03e6cacae439c4ffeccdb68b27100c1ecf507c0
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 15:56:04 +0200

    Fix spec/support/private_api_formatter.rb

--- spec/support/private_api_formatter.rb
+++ spec/support/private_api_formatter.rb
@@ -1,36 +1,38 @@
-module Ably::RSpec
-  # PrivateApiFormatter is an RSpec Formatter that prefixes all tests that are part of a Private API with '(private)'
-  #
-  # Private API methods are tested for this library, but every implementation of the Ably client library
-  # will likely be different and thus the private API method tests are not shared.
-  #
-  # Filter private API tests with `rspec --tag ~api_private`
-  #
-  class PrivateApiFormatter
-    ::RSpec::Core::Formatters.register self, :example_started
+# frozen_string_literal: true
 
-    def initialize(output)
-      @output = output
-    end
+module Ably
+  module RSpec
+    # PrivateApiFormatter is an RSpec Formatter that prefixes all tests that are part of a Private API with '(private)'
+    #
+    # Private API methods are tested for this library, but every implementation of the Ably client library
+    # will likely be different and thus the private API method tests are not shared.
+    #
+    # Filter private API tests with `rspec --tag ~api_private`
+    #
+    class PrivateApiFormatter
+      ::RSpec::Core::Formatters.register self, :example_started
 
-    def example_started(notification)
-      if notification.example.metadata[:api_private]
-        notification.example.metadata[:description] = "#{yellow('(private)')} #{green(notification.example.metadata[:description])}"
+      def initialize(output)
+        @output = output
       end
-    end
 
-    private
-    def colorize(color_code, string)
-      "\e[#{color_code}m#{string}\e[0m"
-    end
+      def example_started(notification)
+        notification.example.metadata[:description] = "#{yellow('(private)')} #{green(notification.example.metadata[:description])}" if notification.example.metadata[:api_private]
+      end
 
-    def yellow(string)
-      colorize(33, string)
-    end
+      private
 
+      def colorize(color_code, string)
+        "\e[#{color_code}m#{string}\e[0m"
+      end
 
-    def green(string)
-      colorize(32, string)
+      def yellow(string)
+        colorize(33, string)
+      end
+
+      def green(string)
+        colorize(32, string)
+      end
     end
   end
 end
