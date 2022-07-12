9eb036a52d42edcacb78e202a4a9284e031e72c2
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 15:32:27 +0200

    Fix spec/unit/models/delta_extras_spec.rb

--- spec/unit/models/delta_extras_spec.rb
+++ spec/unit/models/delta_extras_spec.rb
@@ -1,8 +1,9 @@
-# encoding: utf-8
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe Ably::Models::DeltaExtras do
-  subject { described_class.new({ format: 'vcdiff', from: '1234-4567-8910-1001-1111'}) }
+  subject { described_class.new({ format: 'vcdiff', from: '1234-4567-8910-1001-1111' }) }
 
   it 'should have `from` attribute' do
     expect(subject.from).to eq('1234-4567-8910-1001-1111')
