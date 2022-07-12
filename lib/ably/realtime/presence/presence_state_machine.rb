c8459bc80f9ff2e0f702a90e081a273493c931ac
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:20:17 +0200

    Fix lib/ably/realtime/presence/presence_state_machine.rb

--- lib/ably/realtime/presence/presence_state_machine.rb
+++ lib/ably/realtime/presence/presence_state_machine.rb
@@ -1,56 +1,61 @@
+# frozen_string_literal: true
+
 require 'ably/modules/state_machine'
 
-module Ably::Realtime
-  class Presence
-    # Internal class to manage presence state for {Ably::Realtime::Presence}
-    #
-    # @api private
-    #
-    class PresenceStateMachine
-      include Ably::Modules::StateMachine
+module Ably
+  module Realtime
+    class Presence
+      # Internal class to manage presence state for {Ably::Realtime::Presence}
+      #
+      # @api private
+      #
+      class PresenceStateMachine
+        include ::Ably::Modules::StateMachine
 
-      # States supported by this StateMachine match #{Presence::STATE}s
-      #   :initialized
-      #   :entering
-      #   :entered
-      #   :leaving
-      #   :left
-      Presence::STATE.each_with_index do |state_enum, index|
-        state state_enum.to_sym, initial: index == 0
-      end
+        # States supported by this StateMachine match #{Presence::STATE}s
+        #   :initialized
+        #   :entering
+        #   :entered
+        #   :leaving
+        #   :left
+        Presence::STATE.each_with_index do |state_enum, index|
+          state state_enum.to_sym, initial: index.zero?
+        end
 
-      # Entering or entered states can skip leaving and go straight to left if a channel is detached
-      # A channel that detaches very quickly will also go straight to :left from :initialized
-      transition :from => :initialized,  :to => [:entering, :left]
-      transition :from => :entering,     :to => [:entered, :leaving, :left]
-      transition :from => :entered,      :to => [:leaving, :left]
-      transition :from => :leaving,      :to => [:left, :entering]
+        # Entering or entered states can skip leaving and go straight to left if a channel is detached
+        # A channel that detaches very quickly will also go straight to :left from :initialized
+        transition from: :initialized,  to: %I[entering left]
+        transition from: :entering,     to: %I[entered leaving left]
+        transition from: :entered,      to: %I[leaving left]
+        transition from: :leaving,      to: %I[left entering]
 
-      after_transition do |presence, transition|
-        presence.synchronize_state_with_statemachine
-      end
+        after_transition do |presence, _|
+          presence.synchronize_state_with_statemachine
+        end
 
-      after_transition(to: [:entering]) do |presence, current_transition|
-        presence.manager.enter current_transition.metadata
-      end
+        after_transition(to: [:entering]) do |presence, current_transition|
+          presence.manager.enter current_transition.metadata
+        end
 
-      after_transition(to: [:leaving]) do |presence, current_transition|
-        presence.manager.leave current_transition.metadata
-      end
+        after_transition(to: [:leaving]) do |presence, current_transition|
+          presence.manager.leave current_transition.metadata
+        end
 
-      # Transitions responsible for updating channel#error_reason
-      before_transition(to: [:left]) do |presence, current_transition|
-        presence.channel.set_channel_error_reason current_transition.metadata if is_error_type?(current_transition.metadata)
-      end
+        # Transitions responsible for updating channel#error_reason
+        before_transition(to: [:left]) do |presence, current_transition|
+          presence.channel.set_channel_error_reason current_transition.metadata if is_error_type?(current_transition.metadata)
+        end
 
-      private
-      def channel
-        object.channel
-      end
+        private
 
-      # Logged needs to be defined as it is used by {Ably::Modules::StateMachine}
-      def logger
-        channel.logger
+        def channel
+          object.channel
+        end
+
+        # Logged needs to be defined as it is used by {Ably::Modules::StateMachine}
+        def logger
+          channel.logger
+        end
       end
     end
   end
