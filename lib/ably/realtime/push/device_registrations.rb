be90cc6d8e1ff7109ed52f2a1570dd179d279924
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:19:41 +0200

    Fix lib/ably/realtime/push/device_registrations.rb

--- lib/ably/realtime/push/device_registrations.rb
+++ lib/ably/realtime/push/device_registrations.rb
@@ -1,104 +1,110 @@
-module Ably::Realtime
-  class Push
-    # Manage device registrations for push notifications
-    class DeviceRegistrations
-      include Ably::Modules::Conversions
-      include Ably::Modules::AsyncWrapper
+# frozen_string_literal: true
 
-      # @api private
-      attr_reader :client
+module Ably
+  module Realtime
+    class Push
+      # Manage device registrations for push notifications
+      class DeviceRegistrations
+        include ::Ably::Modules::Conversions
+        include ::Ably::Modules::AsyncWrapper
 
-      # @api private
-      attr_reader :admin
+        # @api private
+        attr_reader :client
 
-      def initialize(admin)
-        @admin = admin
-        @client = admin.client
-      end
+        # @api private
+        attr_reader :admin
 
-      # (see Ably::Rest::Push::DeviceRegistrations#get)
-      #
-      # @yield  Block is invoked when request succeeds
-      # @return [Ably::Util::SafeDeferrable]
-      #
-      def get(device_id, &callback)
-        device_id = device_id.id if device_id.kind_of?(Ably::Models::DeviceDetails)
-        raise ArgumentError, "device_id must be a string or DeviceDetails object" unless device_id.kind_of?(String)
-
-        async_wrap(callback) do
-          rest_device_registrations.get(device_id)
-        end
-      end
-
-      # (see Ably::Rest::Push::DeviceRegistrations#list)
-      #
-      # @yield  Block is invoked when request succeeds
-      # @return [Ably::Util::SafeDeferrable]
-      #
-      def list(params = {}, &callback)
-        params = {} if params.nil?
-        raise ArgumentError, "params must be a Hash" unless params.kind_of?(Hash)
-        raise ArgumentError, "device_id filter cannot be specified alongside a client_id filter. Use one or the other" if params[:client_id] && params[:device_id]
-
-        async_wrap(callback) do
-          rest_device_registrations.list(params.merge(async_blocking_operations: true))
-        end
-      end
-
-      # (see Ably::Rest::Push::DeviceRegistrations#save)
-      #
-      # @yield  Block is invoked when request succeeds
-      # @return [Ably::Util::SafeDeferrable]
-      #
-      def save(device, &callback)
-        device_details = DeviceDetails(device)
-        raise ArgumentError, "Device ID is required yet is empty" if device_details.id.nil? || device_details == ''
-
-        async_wrap(callback) do
-          rest_device_registrations.save(device_details)
-        end
-      end
-
-      # (see Ably::Rest::Push::DeviceRegistrations#remove)
-      #
-      # @yield  Block is invoked when request succeeds
-      # @return [Ably::Util::SafeDeferrable]
-      #
-      def remove(device_id, &callback)
-        device_id = device_id.id if device_id.kind_of?(Ably::Models::DeviceDetails)
-        raise ArgumentError, "device_id must be a string or DeviceDetails object" unless device_id.kind_of?(String)
-
-        async_wrap(callback) do
-          rest_device_registrations.remove(device_id)
-        end
-      end
-
-      # (see Ably::Rest::Push::DeviceRegistrations#remove_where)
-      #
-      # @yield  Block is invoked when request succeeds
-      # @return [Ably::Util::SafeDeferrable]
-      #
-      def remove_where(params = {}, &callback)
-        filter = if params.kind_of?(Ably::Models::DeviceDetails)
-          { 'deviceId' => params.id }
-        else
-          raise ArgumentError, "params must be a Hash" unless params.kind_of?(Hash)
-          raise ArgumentError, "device_id filter cannot be specified alongside a client_id filter. Use one or the other" if params[:client_id] && params[:device_id]
-          IdiomaticRubyWrapper(params).as_json
+        def initialize(admin)
+          @admin = admin
+          @client = admin.client
         end
 
-        async_wrap(callback) do
-          rest_device_registrations.remove_where(filter)
+        # (see Ably::Rest::Push::DeviceRegistrations#get)
+        #
+        # @yield  Block is invoked when request succeeds
+        # @return [Ably::Util::SafeDeferrable]
+        #
+        def get(device_id, &callback)
+          device_id = device_id.id if device_id.is_a?(Ably::Models::DeviceDetails)
+          raise ArgumentError, 'device_id must be a string or DeviceDetails object' unless device_id.is_a?(String)
+
+          async_wrap(callback) do
+            rest_device_registrations.get(device_id)
+          end
         end
-      end
 
-      private
-      def rest_device_registrations
-        client.rest_client.push.admin.device_registrations
-      end
+        # (see Ably::Rest::Push::DeviceRegistrations#list)
+        #
+        # @yield  Block is invoked when request succeeds
+        # @return [Ably::Util::SafeDeferrable]
+        #
+        def list(params = {}, &callback)
+          params = {} if params.nil?
+          raise ArgumentError, 'params must be a Hash' unless params.is_a?(Hash)
+          raise ArgumentError, 'device_id filter cannot be specified alongside a client_id filter. Use one or the other' if params[:client_id] && params[:device_id]
 
-      def logger
-        client.logger
+          async_wrap(callback) do
+            rest_device_registrations.list(params.merge(async_blocking_operations: true))
+          end
+        end
+
+        # (see Ably::Rest::Push::DeviceRegistrations#save)
+        #
+        # @yield  Block is invoked when request succeeds
+        # @return [Ably::Util::SafeDeferrable]
+        #
+        def save(device, &callback)
+          device_details = DeviceDetails(device)
+          raise ArgumentError, 'Device ID is required yet is empty' if device_details.id.nil? || device_details == ''
+
+          async_wrap(callback) do
+            rest_device_registrations.save(device_details)
+          end
+        end
+
+        # (see Ably::Rest::Push::DeviceRegistrations#remove)
+        #
+        # @yield  Block is invoked when request succeeds
+        # @return [Ably::Util::SafeDeferrable]
+        #
+        def remove(device_id, &callback)
+          device_id = device_id.id if device_id.is_a?(Ably::Models::DeviceDetails)
+          raise ArgumentError, 'device_id must be a string or DeviceDetails object' unless device_id.is_a?(String)
+
+          async_wrap(callback) do
+            rest_device_registrations.remove(device_id)
+          end
+        end
+
+        # (see Ably::Rest::Push::DeviceRegistrations#remove_where)
+        #
+        # @yield  Block is invoked when request succeeds
+        # @return [Ably::Util::SafeDeferrable]
+        #
+        def remove_where(params = {}, &callback)
+          filter = if params.is_a?(Ably::Models::DeviceDetails)
+                     { 'deviceId' => params.id }
+                   else
+                     raise ArgumentError, 'params must be a Hash' unless params.is_a?(Hash)
+                     raise ArgumentError, 'device_id filter cannot be specified alongside a client_id filter. Use one or the other' if params[:client_id] && params[:device_id]
+
+                     IdiomaticRubyWrapper(params).as_json
+                   end
+
+          async_wrap(callback) do
+            rest_device_registrations.remove_where(filter)
+          end
+        end
+
+        private
+
+        def rest_device_registrations
+          client.rest_client.push.admin.device_registrations
+        end
+
+        def logger
+          client.logger
+        end
       end
     end
   end
