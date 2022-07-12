1c8cd73e38b45c9af9e0adf21fe30cdd6f3c13dc
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:33:44 +0200

    Fix lib/ably/models/auth_details.rb

--- lib/ably/models/auth_details.rb
+++ lib/ably/models/auth_details.rb
@@ -1,42 +1,48 @@
-module Ably::Models
-  # Convert auth details attributes to a {AuthDetails} object
-  #
-  # @param attributes (see #initialize)
-  #
-  # @return [AuthDetails]
-  def self.AuthDetails(attributes)
-    case attributes
-    when AuthDetails
-      return attributes
-    else
-      AuthDetails.new(attributes || {})
-    end
-  end
+# frozen_string_literal: true
 
-  # AuthDetails are included in an +AUTH+ {Ably::Models::ProtocolMessage#auth} attribute
-  # to provide the realtime service with new token authentication details following a re-auth workflow
+module Ably
+  # Models module provides the methods and classes for the Ably library
   #
-  class AuthDetails
-    include Ably::Modules::ModelCommon
-
-    # @param attributes [Hash]
-    # @option attributes [String]    :access_token     token string
+  module Models
+    # Convert auth details attributes to a {AuthDetails} object
     #
-    def initialize(attributes = {})
-      @hash_object = IdiomaticRubyWrapper(attributes.clone)
-      self.attributes.freeze
-    end
-
-    %w(access_token).each do |attribute|
-      define_method attribute do
-        attributes[attribute.to_sym]
+    # @param attributes (see #initialize)
+    #
+    # @return [AuthDetails]
+    def self.AuthDetails(attributes)
+      case attributes
+      when AuthDetails
+        attributes
+      else
+        AuthDetails.new(attributes || {})
       end
     end
 
-    # @!attribute [r] attributes
-    # @return [Hash] Access the token details Hash object ruby'fied to use symbolized keys
-    def attributes
-      @hash_object
+    # AuthDetails are included in an +AUTH+ {Ably::Models::ProtocolMessage#auth} attribute
+    # to provide the realtime service with new token authentication details following a re-auth workflow
+    #
+    class AuthDetails
+      include ::Ably::Modules::ModelCommon
+
+      # @param attributes [Hash]
+      # @option attributes [String]    :access_token     token string
+      #
+      def initialize(attributes = {})
+        @hash_object = IdiomaticRubyWrapper(attributes.clone)
+        self.attributes.freeze
+      end
+
+      %w[access_token].each do |attribute|
+        define_method attribute do
+          attributes[attribute.to_sym]
+        end
+      end
+
+      # @!attribute [r] attributes
+      # @return [Hash] Access the token details Hash object ruby'fied to use symbolized keys
+      def attributes
+        @hash_object
+      end
     end
   end
 end
