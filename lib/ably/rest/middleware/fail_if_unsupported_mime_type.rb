77a91ea75be81642a7fe9cb3d071d801aca2135f
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:18:10 +0200

    Fix lib/ably/rest/middleware/fail_if_unsupported_mime_type.rb

--- lib/ably/rest/middleware/fail_if_unsupported_mime_type.rb
+++ lib/ably/rest/middleware/fail_if_unsupported_mime_type.rb
@@ -1,20 +1,20 @@
+# frozen_string_literal: true
+
 require 'faraday'
 require 'json'
 
 module Ably
   module Rest
     module Middleware
+      # FailIfUnsupportedMimeType provides the top-level class to be instanced for the Ably library
+      #
       class FailIfUnsupportedMimeType < Faraday::Middleware
         def on_complete(env)
-          unless env.response_headers['Ably-Middleware-Parsed'] == true
-            # Ignore empty body with success status code for no body response
-            return if env.body.to_s.empty? && env.status == 204
+          return if env.response_headers['Ably-Middleware-Parsed'] == true
+          # Ignore empty body with success status code for no body response
+          return if env.body.to_s.empty? && env.status == 204
 
-            unless (500..599).include?(env.status)
-              raise Ably::Exceptions::InvalidResponseBody,
-                    "Content Type #{env.response_headers['Content-Type']} is not supported by this client library"
-            end
-          end
+          raise Ably::Exceptions::InvalidResponseBody, "Content Type #{env.response_headers['Content-Type']} is not supported by this client library" unless (500..599).include?(env.status)
         end
       end
     end
