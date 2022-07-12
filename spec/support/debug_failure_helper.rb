55216998807a25989865f04208df6cc36f3aa5a2
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 15:56:42 +0200

    Fix spec/support/debug_failure_helper.rb

--- spec/support/debug_failure_helper.rb
+++ spec/support/debug_failure_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.configure do |config|
   config.before(:example) do |example|
     next if example.metadata[:prevent_log_stubbing]
@@ -5,7 +7,7 @@ RSpec.configure do |config|
     log_mutex = Mutex.new
 
     @log_output = []
-    %w(fatal error warn info debug).each do |method_name|
+    %w[fatal error warn info debug].each do |method_name|
       allow_any_instance_of(Ably::Logger).to receive(method_name.to_sym).and_wrap_original do |method, *args, &block|
         # Don't log shutdown sequence to keep log noise to a minimum
         next if RSpec.const_defined?(:EventMachine) && RSpec::EventMachine.reactor_stopping?
@@ -13,12 +15,10 @@ RSpec.configure do |config|
         prefix = "#{Time.now.strftime('%H:%M:%S.%L')} [\e[33m#{method_name}\e[0m] "
 
         log_mutex.synchronize do
-          begin
-            args << block.call unless block.nil?
-            @log_output << "#{prefix}#{args.compact.join(' ')}"
-          rescue StandardError => e
-            @log_output << "#{prefix}Failed to log block - #{e.class}: #{e.message}\n#{e.backtrace.join("\n")}}"
-          end
+          args << block.call unless block.nil?
+          @log_output << "#{prefix}#{args.compact.join(' ')}"
+        rescue StandardError => e
+          @log_output << "#{prefix}Failed to log block - #{e.class}: #{e.message}\n#{e.backtrace.join("\n")}}"
         end
 
         # Call original
@@ -31,6 +31,6 @@ RSpec.configure do |config|
     next if example.metadata[:prevent_log_stubbing]
 
     exception = example.exception
-    puts "\n#{'-'*34}\n\e[36mVerbose Ably log from test failure\e[0m\n#{'-'*34}\n#{@log_output.join("\n")}\n\n" if exception
+    puts "\n#{'-' * 34}\n\e[36mVerbose Ably log from test failure\e[0m\n#{'-' * 34}\n#{@log_output.join("\n")}\n\n" if exception
   end
 end
