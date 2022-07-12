1da6bfcea5fc99076827d75125cf44869a1621cb
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:22:21 +0200

    Fix lib/ably/realtime/client/incoming_message_dispatcher.rb

--- lib/ably/realtime/client/incoming_message_dispatcher.rb
+++ lib/ably/realtime/client/incoming_message_dispatcher.rb
@@ -1,65 +1,56 @@
-module Ably::Realtime
-  class Client
-    # IncomingMessageDispatcher is a (private) class that is used to dispatch {Ably::Models::ProtocolMessage} that are
-    # received from Ably via the {Ably::Realtime::Connection}
-    class IncomingMessageDispatcher
-      ACTION = Ably::Models::ProtocolMessage::ACTION
+# frozen_string_literal: true
 
-      def initialize(client, connection)
-        @client     = client
-        @connection = connection
+module Ably
+  module Realtime
+    class Client
+      # IncomingMessageDispatcher is a (private) class that is used to dispatch {Ably::Models::ProtocolMessage} that are
+      # received from Ably via the {Ably::Realtime::Connection}
+      class IncomingMessageDispatcher
+        ACTION = ::Ably::Models::ProtocolMessage::ACTION
 
-        subscribe_to_incoming_protocol_messages
-      end
+        def initialize(client, connection)
+          @client     = client
+          @connection = connection
 
-      private
-      def client
-        @client
-      end
-
-      def connection
-        @connection
-      end
-
-      def channels
-        client.channels
-      end
-
-      def get_channel(channel_name)
-        channels.fetch(channel_name) do
-          logger.warn { "Received channel message for non-existent channel" }
-          Ably::Realtime::Models::NilChannel.new
-        end
-      end
-
-      def logger
-        client.logger
-      end
-
-      def dispatch_protocol_message(*args)
-        protocol_message = args.first
-
-        unless protocol_message.kind_of?(Ably::Models::ProtocolMessage)
-          raise ArgumentError, "Expected a ProtocolMessage. Received #{protocol_message}"
+          subscribe_to_incoming_protocol_messages
         end
 
-        unless protocol_message.action.match_any?(:nack, :error)
-          logger.debug { "#{protocol_message.action} received: #{protocol_message}" }
+        private
+
+        attr_reader :client, :connection
+
+        def channels
+          client.channels
         end
 
-        if protocol_message.action.match_any?(:sync, :presence, :message)
-          if connection.serial && protocol_message.has_connection_serial? && protocol_message.connection_serial <= connection.serial
+        def get_channel(channel_name)
+          channels.fetch(channel_name) do
+            logger.warn { 'Received channel message for non-existent channel' }
+            ::Ably::Realtime::Models::NilChannel.new
+          end
+        end
+
+        def logger
+          client.logger
+        end
+
+        def dispatch_protocol_message(*args)
+          protocol_message = args.first
+          raise ArgumentError, "Expected a ProtocolMessage. Received #{protocol_message}" unless protocol_message.is_a?(::Ably::Models::ProtocolMessage)
+          logger.debug { "#{protocol_message.action} received: #{protocol_message}" } unless protocol_message.action.match_any?(:nack, :error)
+
+          if protocol_message.action.match_any?(:sync, :presence, :message) && connection.serial && protocol_message.has_connection_serial? && protocol_message.connection_serial <= connection.serial
             error_message = "Protocol error, duplicate message received for serial #{protocol_message.connection_serial}"
             logger.error error_message
             return
           end
-        end
 
-        update_connection_recovery_info protocol_message
-        connection.set_connection_confirmed_alive
+          update_connection_recovery_info protocol_message
+          connection.set_connection_confirmed_alive
 
-        case protocol_message.action
+          case protocol_message.action
           when ACTION.Heartbeat
+            # nothing
           when ACTION.Ack
             ack_pending_queue_for_message_serial(protocol_message) if protocol_message.has_message_serial?
 
@@ -68,13 +59,14 @@ module Ably::Realtime
             nack_pending_queue_for_message_serial(protocol_message) if protocol_message.has_message_serial?
 
           when ACTION.Connect
+            # nothing
           when ACTION.Connected
             if connection.closing?
               logger.debug { "Out-of-order incoming CONNECTED ProtocolMessage discarded as connection has moved on and is in state: #{connection.state}" }
             elsif connection.disconnected? || connection.closing? || connection.closed? || connection.failed?
               logger.warn { "Out-of-order incoming CONNECTED ProtocolMessage discarded as connection has moved on and is in state: #{connection.state}" }
             elsif connection.connected?
-              logger.debug { "Updated CONNECTED ProtocolMessage received (whilst connected)" }
+              logger.debug { 'Updated CONNECTED ProtocolMessage received (whilst connected)' }
               process_connected_update_message protocol_message
               connection.set_connection_confirmed_alive # Connection protocol messages can change liveness settings such as max_idle_interval
             else
@@ -86,6 +78,7 @@ module Ably::Realtime
             connection.transition_state_machine :disconnected, reason: protocol_message.error unless connection.disconnected?
 
           when ACTION.Close
+            # nothing
           when ACTION.Closed
             connection.transition_state_machine :closed unless connection.closed?
 
@@ -97,20 +90,20 @@ module Ably::Realtime
             end
 
           when ACTION.Attach
+            # nothing
           when ACTION.Attached
             get_channel(protocol_message.channel).tap do |channel|
               if channel.attached?
                 channel.manager.duplicate_attached_received protocol_message
+              elsif channel.failed?
+                logger.warn "Ably::Realtime::Client::IncomingMessageDispatcher - Received an ATTACHED protocol message for FAILED channel #{channel.name}. Ignoring ATTACHED message"
               else
-                if channel.failed?
-                  logger.warn "Ably::Realtime::Client::IncomingMessageDispatcher - Received an ATTACHED protocol message for FAILED channel #{channel.name}. Ignoring ATTACHED message"
-                else
-                  channel.transition_state_machine :attached, reason: protocol_message.error, resumed: protocol_message.has_channel_resumed_flag?, protocol_message: protocol_message
-                end
+                channel.transition_state_machine :attached, reason: protocol_message.error, resumed: protocol_message.has_channel_resumed_flag?, protocol_message: protocol_message
               end
             end
 
           when ACTION.Detach
+            # nothing
           when ACTION.Detached
             get_channel(protocol_message.channel).tap do |channel|
               channel.manager.detached_received protocol_message.error
@@ -146,85 +139,87 @@ module Ably::Realtime
           else
             error = Ably::Exceptions::ProtocolError.new("Protocol Message Action #{protocol_message.action} is unsupported by this MessageDispatcher", 400, Ably::Exceptions::Codes::PROTOCOL_ERROR)
             logger.fatal error.message
+          end
         end
-      end
 
-      def dispatch_channel_error(protocol_message)
-        logger.warn { "Channel Error message received: #{protocol_message.error}" }
-        if !protocol_message.has_message_serial?
-          get_channel(protocol_message.channel).transition_state_machine :failed, reason: protocol_message.error
-        else
-          logger.fatal { "Cannot process ProtocolMessage ERROR with message serial as not yet implemented: #{protocol_message}" }
+        def dispatch_channel_error(protocol_message)
+          logger.warn { "Channel Error message received: #{protocol_message.error}" }
+          if !protocol_message.has_message_serial?
+            get_channel(protocol_message.channel).transition_state_machine :failed, reason: protocol_message.error
+          else
+            logger.fatal { "Cannot process ProtocolMessage ERROR with message serial as not yet implemented: #{protocol_message}" }
+          end
         end
-      end
 
-      def process_connection_error(protocol_message)
-        connection.manager.error_received_from_server(protocol_message.error || Ably::Models::ErrorInfo.new(message: 'Error reason unknown'))
-      end
-
-      def process_connected_message(protocol_message)
-        if client.auth.token_client_id_allowed?(protocol_message.connection_details.client_id)
-          connection.transition_state_machine :connected, reason: protocol_message.error, protocol_message: protocol_message
-        else
-          reason = Ably::Exceptions::IncompatibleClientId.new("Client ID '#{protocol_message.connection_details.client_id}' specified by the server is incompatible with the library's configured client ID '#{client.client_id}'")
-          connection.transition_state_machine :failed, reason: reason, protocol_message: protocol_message
+        def process_connection_error(protocol_message)
+          connection.manager.error_received_from_server(protocol_message.error || Ably::Models::ErrorInfo.new(message: 'Error reason unknown'))
         end
-      end
 
-      def process_connected_update_message(protocol_message)
-        if client.auth.token_client_id_allowed?(protocol_message.connection_details.client_id)
-          connection.manager.connected_update protocol_message
-        else
-          reason = Ably::Exceptions::IncompatibleClientId.new("Client ID '#{protocol_message.connection_details.client_id}' in CONNECTED update specified by the server is incompatible with the library's configured client ID '#{client.client_id}'")
-          connection.transition_state_machine :failed, reason: reason, protocol_message: protocol_message
+        def process_connected_message(protocol_message)
+          if client.auth.token_client_id_allowed?(protocol_message.connection_details.client_id)
+            connection.transition_state_machine :connected, reason: protocol_message.error, protocol_message: protocol_message
+          else
+            reason = Ably::Exceptions::IncompatibleClientId.new("Client ID '#{protocol_message.connection_details.client_id}' specified by the server is incompatible with the library's configured client ID '#{client.client_id}'")
+            connection.transition_state_machine :failed, reason: reason, protocol_message: protocol_message
+          end
         end
-      end
 
-      def update_connection_recovery_info(protocol_message)
-        connection.update_connection_serial protocol_message.connection_serial if protocol_message.has_connection_serial?
-      end
-
-      def ack_pending_queue_for_message_serial(ack_protocol_message)
-        drop_pending_queue_from_ack(ack_protocol_message) do |protocol_message|
-          ack_messages protocol_message.messages
-          ack_messages protocol_message.presence
+        def process_connected_update_message(protocol_message)
+          if client.auth.token_client_id_allowed?(protocol_message.connection_details.client_id)
+            connection.manager.connected_update protocol_message
+          else
+            reason = Ably::Exceptions::IncompatibleClientId.new("Client ID '#{protocol_message.connection_details.client_id}' in CONNECTED update specified by the server is incompatible with the library's configured client ID '#{client.client_id}'")
+            connection.transition_state_machine :failed, reason: reason, protocol_message: protocol_message
+          end
         end
-      end
 
-      def nack_pending_queue_for_message_serial(nack_protocol_message)
-        drop_pending_queue_from_ack(nack_protocol_message) do |protocol_message|
-          nack_messages protocol_message.messages, nack_protocol_message
-          nack_messages protocol_message.presence, nack_protocol_message
+        def update_connection_recovery_info(protocol_message)
+          connection.update_connection_serial protocol_message.connection_serial if protocol_message.has_connection_serial?
         end
-      end
 
-      def ack_messages(messages)
-        messages.each do |message|
-          logger.debug { "Calling ACK success callbacks for #{message.class.name} - #{message.to_json}" }
-          message.succeed message
+        def ack_pending_queue_for_message_serial(ack_protocol_message)
+          drop_pending_queue_from_ack(ack_protocol_message) do |protocol_message|
+            ack_messages protocol_message.messages
+            ack_messages protocol_message.presence
+          end
         end
-      end
 
-      def nack_messages(messages, protocol_message)
-        messages.each do |message|
-          logger.debug { "Calling NACK failure callbacks for #{message.class.name} - #{message.to_json}, protocol message: #{protocol_message}" }
-          message.fail protocol_message.error
+        def nack_pending_queue_for_message_serial(nack_protocol_message)
+          drop_pending_queue_from_ack(nack_protocol_message) do |protocol_message|
+            nack_messages protocol_message.messages, nack_protocol_message
+            nack_messages protocol_message.presence, nack_protocol_message
+          end
         end
-      end
 
-      def drop_pending_queue_from_ack(ack_protocol_message)
-        message_serial_up_to = ack_protocol_message.message_serial + ack_protocol_message.count - 1
-
-        while !connection.__pending_message_ack_queue__.empty?
-          next_message = connection.__pending_message_ack_queue__.first
-          return if next_message.message_serial > message_serial_up_to
-          yield connection.__pending_message_ack_queue__.shift
+        def ack_messages(messages)
+          messages.each do |message|
+            logger.debug { "Calling ACK success callbacks for #{message.class.name} - #{message.to_json}" }
+            message.succeed message
+          end
         end
-      end
 
-      def subscribe_to_incoming_protocol_messages
-        connection.__incoming_protocol_msgbus__.subscribe(:protocol_message) do |*args|
-          dispatch_protocol_message(*args)
+        def nack_messages(messages, protocol_message)
+          messages.each do |message|
+            logger.debug { "Calling NACK failure callbacks for #{message.class.name} - #{message.to_json}, protocol message: #{protocol_message}" }
+            message.fail protocol_message.error
+          end
+        end
+
+        def drop_pending_queue_from_ack(ack_protocol_message)
+          message_serial_up_to = ack_protocol_message.message_serial + ack_protocol_message.count - 1
+
+          while !connection.__pending_message_ack_queue__.empty?
+            next_message = connection.__pending_message_ack_queue__.first
+            return if next_message.message_serial > message_serial_up_to
+
+            yield connection.__pending_message_ack_queue__.shift
+          end
+        end
+
+        def subscribe_to_incoming_protocol_messages
+          connection.__incoming_protocol_msgbus__.subscribe(:protocol_message) do |*args|
+            dispatch_protocol_message(*args)
+          end
         end
       end
     end
