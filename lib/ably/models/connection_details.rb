8722778c83d3c201ac7ebcc17566c8280e3b733b
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:32:26 +0200

    Fix lib/ably/models/connection_details.rb

--- lib/ably/models/connection_details.rb
+++ lib/ably/models/connection_details.rb
@@ -1,68 +1,72 @@
-module Ably::Models
-  # Convert connection details attributes to a {ConnectionDetails} object
+# frozen_string_literal: true
+
+module Ably
+  # Models module provides the methods and classes for the Ably library
   #
-  # @param attributes (see #initialize)
-  #
-  # @return [ConnectionDetails]
-  def self.ConnectionDetails(attributes)
-    case attributes
-    when ConnectionDetails
-      return attributes
-    else
-      ConnectionDetails.new(attributes || {})
-    end
-  end
-
-  # ConnectionDetails are optionally passed to the client library in the +CONNECTED+ {Ably::Models::ProtocolMessage#connectionDetails} attribute
-  # to inform the client about any constraints it should adhere to and provide additional metadata about the connection.
-  # For example, if a request is made to publish a message that exceeds the +maxMessageSize+, the client library can reject
-  # the message immediately, without communicating with the Ably service
-  #
-  class ConnectionDetails
-    include Ably::Modules::ModelCommon
-
-    # Max message size
-    MAX_MESSAGE_SIZE = 65536 # See spec TO3l8
-
-    # Max frame size
-    MAX_FRAME_SIZE = 524288 # See spec TO3l9
-
-    # @param attributes [Hash]
-    # @option attributes [String]    :client_id             contains the client ID assigned to the connection
-    # @option attributes [String]    :connection_key        the connection secret key string that is used to resume a connection and its state
-    # @option attributes [Integer]   :max_message_size      maximum individual message size in bytes
-    # @option attributes [Integer]   :max_frame_size        maximum size for a single frame of data sent to Ably. This restriction applies to a {Ably::Models::ProtocolMessage} sent over a realtime connection, or the total body size for a REST request
-    # @option attributes [Integer]   :max_inbound_rate      maximum allowable number of requests per second from a client
-    # @option attributes [Integer]   :max_idle_interval     is the maximum length of time in seconds that the server will allow no activity to occur in the server->client direction. After such a period of inactivity, the server will send a @HEARTBEAT@ or transport-level ping to the client. If the value is 0, the server will allow arbitrarily-long levels of inactivity.
-    # @option attributes [Integer]   :connection_state_ttl  duration in seconds that Ably will persist the connection state when a Realtime client is abruptly disconnected
-    # @option attributes [String]    :server_id             unique identifier of the Ably server where the connection is established
+  module Models
+    # Convert connection details attributes to a {ConnectionDetails} object
     #
-    def initialize(attributes = {})
-      @hash_object = IdiomaticRubyWrapper(attributes.clone)
-      [:connection_state_ttl, :max_idle_interval].each do |duration_field|
-        if self.attributes[duration_field]
-          self.attributes[duration_field] = (self.attributes[duration_field].to_f / 1000).round
+    # @param attributes (see #initialize)
+    #
+    # @return [ConnectionDetails]
+    def self.ConnectionDetails(attributes)
+      case attributes
+      when ConnectionDetails
+        attributes
+      else
+        ConnectionDetails.new(attributes || {})
+      end
+    end
+
+    # ConnectionDetails are optionally passed to the client library in the +CONNECTED+ {Ably::Models::ProtocolMessage#connectionDetails} attribute
+    # to inform the client about any constraints it should adhere to and provide additional metadata about the connection.
+    # For example, if a request is made to publish a message that exceeds the +maxMessageSize+, the client library can reject
+    # the message immediately, without communicating with the Ably service
+    #
+    class ConnectionDetails
+      include ::Ably::Modules::ModelCommon
+
+      # Max message size
+      MAX_MESSAGE_SIZE = 65_536 # See spec TO3l8
+
+      # Max frame size
+      MAX_FRAME_SIZE = 524_288 # See spec TO3l9
+
+      # @param attributes [Hash]
+      # @option attributes [String]    :client_id             contains the client ID assigned to the connection
+      # @option attributes [String]    :connection_key        the connection secret key string that is used to resume a connection and its state
+      # @option attributes [Integer]   :max_message_size      maximum individual message size in bytes
+      # @option attributes [Integer]   :max_frame_size        maximum size for a single frame of data sent to Ably. This restriction applies to a {Ably::Models::ProtocolMessage} sent over a realtime connection, or the total body size for a REST request
+      # @option attributes [Integer]   :max_inbound_rate      maximum allowable number of requests per second from a client
+      # @option attributes [Integer]   :max_idle_interval     is the maximum length of time in seconds that the server will allow no activity to occur in the server->client direction. After such a period of inactivity, the server will send a @HEARTBEAT@ or transport-level ping to the client. If the value is 0, the server will allow arbitrarily-long levels of inactivity.
+      # @option attributes [Integer]   :connection_state_ttl  duration in seconds that Ably will persist the connection state when a Realtime client is abruptly disconnected
+      # @option attributes [String]    :server_id             unique identifier of the Ably server where the connection is established
+      #
+      def initialize(attributes = {})
+        @hash_object = IdiomaticRubyWrapper(attributes.clone)
+        %I[connection_state_ttl max_idle_interval].each do |duration_field|
+          self.attributes[duration_field] = (self.attributes[duration_field].to_f / 1000).round if self.attributes[duration_field]
+        end
+        self.attributes[:max_message_size] ||= MAX_MESSAGE_SIZE
+        self.attributes[:max_frame_size] ||= MAX_FRAME_SIZE
+        self.attributes.freeze
+      end
+
+      %w[client_id connection_key max_message_size max_frame_size max_inbound_rate connection_state_ttl max_idle_interval server_id].each do |attribute|
+        define_method attribute do
+          attributes[attribute.to_sym]
         end
       end
-      self.attributes[:max_message_size] ||= MAX_MESSAGE_SIZE
-      self.attributes[:max_frame_size] ||= MAX_FRAME_SIZE
-      self.attributes.freeze
-    end
 
-    %w(client_id connection_key max_message_size max_frame_size max_inbound_rate connection_state_ttl max_idle_interval server_id).each do |attribute|
-      define_method attribute do
-        attributes[attribute.to_sym]
+      def has_client_id?
+        attributes.key?(:client_id)
       end
-    end
 
-    def has_client_id?
-      attributes.has_key?(:client_id)
-    end
-
-    # @!attribute [r] attributes
-    # @return [Hash] Access the token details Hash object ruby'fied to use symbolized keys
-    def attributes
-      @hash_object
+      # @!attribute [r] attributes
+      # @return [Hash] Access the token details Hash object ruby'fied to use symbolized keys
+      def attributes
+        @hash_object
+      end
     end
   end
 end
