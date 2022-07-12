180b84e294f40e9b3dada04bc0ed6659821cdece
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:33:09 +0200

    Fix lib/ably/models/channel_occupancy.rb

--- lib/ably/models/channel_occupancy.rb
+++ lib/ably/models/channel_occupancy.rb
@@ -1,43 +1,49 @@
-module Ably::Models
-  # Convert token details argument to a {ChannelOccupancy} object
-  #
-  # @param attributes (see #initialize)
-  #
-  # @return [ChannelOccupancy]
-  def self.ChannelOccupancy(attributes)
-    case attributes
-    when ChannelOccupancy
-      return attributes
-    else
-      ChannelOccupancy.new(attributes)
-    end
-  end
+# frozen_string_literal: true
 
-  # Type that contain channel metrics (CHO1)
+module Ably
+  # Models module provides the methods and classes for the Ably library
   #
-  class ChannelOccupancy
-    extend Ably::Modules::Enum
-    extend Forwardable
-    include Ably::Modules::ModelCommon
-
-    # The attributes of ChannelOccupancy (CH02)
+  module Models
+    # Convert token details argument to a {ChannelOccupancy} object
     #
-    attr_reader :attributes
-
-    alias_method :to_h, :attributes
-
-    # Initialize a new ChannelOccupancy
+    # @param attributes (see #initialize)
     #
-    def initialize(attrs)
-      @attributes = IdiomaticRubyWrapper(attrs.clone)
+    # @return [ChannelOccupancy]
+    def self.ChannelOccupancy(attributes)
+      case attributes
+      when ChannelOccupancy
+        attributes
+      else
+        ChannelOccupancy.new(attributes)
+      end
     end
 
-    # Metrics object (CHO2a)
+    # Type that contain channel metrics (CHO1)
     #
-    # @return [Ably::Models::ChannelMetrics, nil]
-    #
-    def metrics
-      Ably::Models::ChannelMetrics(attributes[:metrics])
+    class ChannelOccupancy
+      extend ::Ably::Modules::Enum
+      extend Forwardable
+      include ::Ably::Modules::ModelCommon
+
+      # The attributes of ChannelOccupancy (CH02)
+      #
+      attr_reader :attributes
+
+      alias_method :to_h, :attributes
+
+      # Initialize a new ChannelOccupancy
+      #
+      def initialize(attrs)
+        @attributes = IdiomaticRubyWrapper(attrs.clone)
+      end
+
+      # Metrics object (CHO2a)
+      #
+      # @return [Ably::Models::ChannelMetrics, nil]
+      #
+      def metrics
+        Ably::Models::ChannelMetrics(attributes[:metrics])
+      end
     end
   end
 end
