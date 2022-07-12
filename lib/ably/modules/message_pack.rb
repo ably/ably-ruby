d75c273ad7b74c0884eeae54cb3e10946ae4ef97
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:25:11 +0200

    Fix lib/ably/modules/message_pack.rb

--- lib/ably/modules/message_pack.rb
+++ lib/ably/modules/message_pack.rb
@@ -1,14 +1,18 @@
+# frozen_string_literal: true
+
 require 'msgpack'
 
-module Ably::Modules
-  # MessagePack module adds #to_msgpack to the class on the assumption that the class
-  # supports the method #as_json
-  #
-  module MessagePack
-    # Generate a packed MsgPack version of this object based on the JSON representation.
-    # Keys thus use mixedCase syntax as expected by the Realtime API
-    def to_msgpack(pk = nil)
-      as_json.to_msgpack(pk)
+module Ably
+  module Modules
+    # MessagePack module adds #to_msgpack to the class on the assumption that the class
+    # supports the method #as_json
+    #
+    module MessagePack
+      # Generate a packed MsgPack version of this object based on the JSON representation.
+      # Keys thus use mixedCase syntax as expected by the Realtime API
+      def to_msgpack(pk = nil)
+        as_json.to_msgpack(pk)
+      end
     end
   end
 end
