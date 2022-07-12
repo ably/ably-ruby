ce0daebd33a04b12ee452848cc463ffa54812d39
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:32:42 +0200

    Fix lib/ably/models/channel_status.rb

--- lib/ably/models/channel_status.rb
+++ lib/ably/models/channel_status.rb
@@ -1,53 +1,59 @@
-module Ably::Models
-  # Convert token details argument to a {ChannelStatus} object
-  #
-  # @param attributes (see #initialize)
-  #
-  # @return [ChannelStatus]
-  def self.ChannelStatus(attributes)
-    case attributes
-    when ChannelStatus
-      return attributes
-    else
-      ChannelStatus.new(attributes)
-    end
-  end
+# frozen_string_literal: true
 
-  # ChannelStatus is a type that contains status and occupancy for a channel (CHS1)
+module Ably
+  # Models module provides the methods and classes for the Ably library
   #
-  class ChannelStatus
-    extend Ably::Modules::Enum
-    extend Forwardable
-    include Ably::Modules::ModelCommon
-
-    # The attributes of ChannelStatus (CHS2)
+  module Models
+    # Convert token details argument to a {ChannelStatus} object
     #
-    attr_reader :attributes
-
-    alias_method :to_h, :attributes
-
-    # Initialize a new ChannelStatus
+    # @param attributes (see #initialize)
     #
-    def initialize(attrs)
-      @attributes = IdiomaticRubyWrapper(attrs.clone)
+    # @return [ChannelStatus]
+    def self.ChannelStatus(attributes)
+      case attributes
+      when ChannelStatus
+        attributes
+      else
+        ChannelStatus.new(attributes)
+      end
     end
 
-    # Represents if the channel is active (CHS2a)
+    # ChannelStatus is a type that contains status and occupancy for a channel (CHS1)
     #
-    # @return [Boolean]
-    #
-    def is_active
-      attributes[:isActive]
-    end
-    alias_method :active?, :is_active
-    alias_method :is_active?, :is_active
+    class ChannelStatus
+      extend ::Ably::Modules::Enum
+      extend Forwardable
+      include ::Ably::Modules::ModelCommon
 
-    # Occupancy ChannelOccupancy – occupancy is an object containing the metrics for the channel (CHS2b)
-    #
-    # @return [Ably::Models::ChannelOccupancy, nil]
-    #
-    def occupancy
-      Ably::Models::ChannelOccupancy(attributes[:occupancy])
+      # The attributes of ChannelStatus (CHS2)
+      #
+      attr_reader :attributes
+
+      alias_method :to_h, :attributes
+
+      # Initialize a new ChannelStatus
+      #
+      def initialize(attrs)
+        @attributes = IdiomaticRubyWrapper(attrs.clone)
+      end
+
+      # Represents if the channel is active (CHS2a)
+      #
+      # @return [Boolean]
+      #
+      def is_active
+        attributes[:isActive]
+      end
+      alias_method :active?, :is_active
+      alias_method :is_active?, :is_active
+
+      # Occupancy ChannelOccupancy – occupancy is an object containing the metrics for the channel (CHS2b)
+      #
+      # @return [Ably::Models::ChannelOccupancy, nil]
+      #
+      def occupancy
+        Ably::Models::ChannelOccupancy(attributes[:occupancy])
+      end
     end
   end
 end
