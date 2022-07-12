fc3894907f5a7d6068034c90625e787f0885704b
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:16:04 +0200

    Fix lib/ably/util/crypto.rb

--- lib/ably/util/crypto.rb
+++ lib/ably/util/crypto.rb
@@ -1,130 +1,137 @@
+# frozen_string_literal: true
+
 require 'msgpack'
 require 'openssl'
 
-module Ably::Util
-  class Crypto
-    DEFAULTS = {
-      algorithm: 'aes',
-      mode: 'cbc',
-      key_length: 256,
-    }
-
-    BLOCK_LENGTH = 16
-
-    # Configured {Ably::Models::CipherParams} for this Crypto object, see {#initialize} for a list of configureable options
+module Ably
+  module Util
+    # Crypto provides the top-level class to be instanced for the Ably library
     #
-    # @return [Ably::Models::CipherParams]
-    attr_reader :cipher_params
+    class Crypto
+      DEFAULTS = {
+        algorithm: 'aes',
+        mode: 'cbc',
+        key_length: 256
+      }.freeze
 
-    # Creates a {Ably::Util::Crypto} object
-    #
-    # @param [Hash] params a Hash used to configure the Crypto library's {Ably::Models::CipherParams}
-    # @option params (see Ably::Models::CipherParams#initialize)
-    #
-    # @return [Ably::Util::Crypto]
-    #
-    # @example
-    #    key = Ably::Util::Crypto.generate_random_key
-    #    crypto = Ably::Util::Crypto.new(key: key)
-    #    encrypted = crypto.encrypt('secret text')
-    #    crypto.decrypt(decrypted) # => 'secret text'
-    #
-    def initialize(params)
-      @fixed_iv = params[:fixed_iv]
-      @cipher_params = Ably::Models::CipherParams(params)
-    end
+      BLOCK_LENGTH = 16
 
-    # Obtain a default {Ably::Models::CipherParams}. This uses default algorithm, mode and
-    # padding and key length. An IV is generated using the default
-    # system SecureRandom; the key may be obtained from the returned {Ably::Models::CipherParams}
-    # for out-of-band distribution to other clients.
+      # Configured {Ably::Models::CipherParams} for this Crypto object, see {#initialize} for a list of configureable options
+      #
+      # @return [Ably::Models::CipherParams]
+      attr_reader :cipher_params
 
-    # @param [Hash]  params  a Hash used to configure the Crypto library's {Ably::Models::CipherParams}
-    # @option params  (see Ably::Models::CipherParams#initialize)
-    #
-    # @return [Ably::Models::CipherParams]   Configured cipher params with :key, :algorithm, :mode, :key_length attributes
-    #
-    def self.get_default_params(params = {})
-      Ably::Models::CipherParams(params)
-    end
+      # Creates a {Ably::Util::Crypto} object
+      #
+      # @param [Hash] params a Hash used to configure the Crypto library's {Ably::Models::CipherParams}
+      # @option params (see Ably::Models::CipherParams#initialize)
+      #
+      # @return [Ably::Util::Crypto]
+      #
+      # @example
+      #    key = Ably::Util::Crypto.generate_random_key
+      #    crypto = Ably::Util::Crypto.new(key: key)
+      #    encrypted = crypto.encrypt('secret text')
+      #    crypto.decrypt(decrypted) # => 'secret text'
+      #
+      def initialize(params)
+        @fixed_iv = params[:fixed_iv]
+        @cipher_params = Ably::Models::CipherParams(params)
+      end
 
-    # Generate a random encryption key from the supplied keylength (or the
-    # default key_length of 256 if none supplied)
-    #
-    # @param  [Integer]  key_length  Optional (default 256) key length for the generated random key. 128 and 256 bit key lengths are supported
-    # @return   Binary   String (byte array) with ASCII_8BIT encoding
-    #
-    def self.generate_random_key(key_length = DEFAULTS.fetch(:key_length))
-      params = DEFAULTS.merge(key_length: key_length)
-      OpenSSL::Cipher.new(cipher_type(params)).random_key
-    end
+      # Obtain a default {Ably::Models::CipherParams}. This uses default algorithm, mode and
+      # padding and key length. An IV is generated using the default
+      # system SecureRandom; the key may be obtained from the returned {Ably::Models::CipherParams}
+      # for out-of-band distribution to other clients.
 
-    # The Cipher algorithm string such as AES-128-CBC
-    # @api private
-    def self.cipher_type(options)
-      Ably::Models::CipherParams.cipher_type(options)
-    end
+      # @param [Hash]  params  a Hash used to configure the Crypto library's {Ably::Models::CipherParams}
+      # @option params  (see Ably::Models::CipherParams#initialize)
+      #
+      # @return [Ably::Models::CipherParams]   Configured cipher params with :key, :algorithm, :mode, :key_length attributes
+      #
+      def self.get_default_params(params = {})
+        Ably::Models::CipherParams(params)
+      end
 
-    # Encrypt payload using configured Cipher
-    #
-    # @param [String] payload           the payload to be encrypted
-    # @param [Hash]   encrypt_options   an options Hash to configure the encrypt action
-    # @option encrypt_options [String]  :iv optionally use the provided Initialization Vector instead of a randomly generated IV
-    #
-    # @return [String] binary string with +Encoding::ASCII_8BIT+ encoding
-    #
-    def encrypt(payload, encrypt_options = {})
-      cipher = openssl_cipher
-      cipher.encrypt
-      cipher.key = key
-      iv = encrypt_options[:iv] || fixed_iv || cipher.random_iv
-      cipher.iv = iv
+      # Generate a random encryption key from the supplied keylength (or the
+      # default key_length of 256 if none supplied)
+      #
+      # @param  [Integer]  key_length  Optional (default 256) key length for the generated random key. 128 and 256 bit key lengths are supported
+      # @return   Binary   String (byte array) with ASCII_8BIT encoding
+      #
+      def self.generate_random_key(key_length = DEFAULTS.fetch(:key_length))
+        params = DEFAULTS.merge(key_length: key_length)
+        OpenSSL::Cipher.new(cipher_type(params)).random_key
+      end
 
-      iv << cipher.update(payload) << cipher.final
-    end
+      # The Cipher algorithm string such as AES-128-CBC
+      # @api private
+      def self.cipher_type(options)
+        Ably::Models::CipherParams.cipher_type(options)
+      end
 
-    # Decrypt payload using configured Cipher
-    #
-    # @param [String] encrypted_payload_with_iv  the encrypted payload to be decrypted
-    #
-    # @return [String]
-    #
-    def decrypt(encrypted_payload_with_iv)
-      raise Ably::Exceptions::CipherError, 'iv is missing or not long enough' unless encrypted_payload_with_iv.length >= BLOCK_LENGTH*2
+      # Encrypt payload using configured Cipher
+      #
+      # @param [String] payload           the payload to be encrypted
+      # @param [Hash]   encrypt_options   an options Hash to configure the encrypt action
+      # @option encrypt_options [String]  :iv optionally use the provided Initialization Vector instead of a randomly generated IV
+      #
+      # @return [String] binary string with +Encoding::ASCII_8BIT+ encoding
+      #
+      def encrypt(payload, encrypt_options = {})
+        cipher = openssl_cipher
+        cipher.encrypt
+        cipher.key = key
+        iv = encrypt_options[:iv] || fixed_iv || cipher.random_iv
+        cipher.iv = iv
 
-      iv = encrypted_payload_with_iv.slice(0...BLOCK_LENGTH)
-      encrypted_payload = encrypted_payload_with_iv.slice(BLOCK_LENGTH..-1)
+        iv << cipher.update(payload) << cipher.final
+      end
 
-      decipher = openssl_cipher
-      decipher.decrypt
-      decipher.key = key
-      decipher.iv = iv
+      # Decrypt payload using configured Cipher
+      #
+      # @param [String] encrypted_payload_with_iv  the encrypted payload to be decrypted
+      #
+      # @return [String]
+      #
+      def decrypt(encrypted_payload_with_iv)
+        raise Ably::Exceptions::CipherError, 'iv is missing or not long enough' unless encrypted_payload_with_iv.length >= BLOCK_LENGTH * 2
 
-      decipher.update(encrypted_payload) << decipher.final
-    end
+        iv = encrypted_payload_with_iv.slice(0...BLOCK_LENGTH)
+        encrypted_payload = encrypted_payload_with_iv.slice(BLOCK_LENGTH..-1)
 
-    # Generate a random IV
-    # @return [String]
-    def random_iv
-      openssl_cipher.random_iv
-    end
+        decipher = openssl_cipher
+        decipher.decrypt
+        decipher.key = key
+        decipher.iv = iv
 
-    private
-    # Used solely for tests to fix the IV instead of randomly generate one
-    attr_reader :fixed_iv
+        decipher.update(encrypted_payload) << decipher.final
+      end
 
-    # Generate a random key
-    # @return [String]
-    def random_key
-      openssl_cipher.random_key
-    end
+      # Generate a random IV
+      # @return [String]
+      def random_iv
+        openssl_cipher.random_iv
+      end
 
-    def key
-      cipher_params.key
-    end
+      private
 
-    def openssl_cipher
-      @openssl_cipher ||= OpenSSL::Cipher.new(cipher_params.cipher_type)
+      # Used solely for tests to fix the IV instead of randomly generate one
+      attr_reader :fixed_iv
+
+      # Generate a random key
+      # @return [String]
+      def random_key
+        openssl_cipher.random_key
+      end
+
+      def key
+        cipher_params.key
+      end
+
+      def openssl_cipher
+        @openssl_cipher ||= OpenSSL::Cipher.new(cipher_params.cipher_type)
+      end
     end
   end
 end
