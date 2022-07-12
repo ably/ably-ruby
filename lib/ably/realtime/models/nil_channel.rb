b7d0d40837f3a202f58c5698f6e1d04c7529ac52
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:21:15 +0200

    Fix lib/ably/realtime/models/nil_channel.rb

--- lib/ably/realtime/models/nil_channel.rb
+++ lib/ably/realtime/models/nil_channel.rb
@@ -1,30 +1,36 @@
-module Ably::Realtime::Models
-  # Nil object for Channels, this object is only used within the internal API of this client library
-  # @api private
-  class NilChannel
-    include Ably::Modules::EventEmitter
-    extend Ably::Modules::Enum
-    STATE = ruby_enum('STATE', Ably::Realtime::Channel::STATE)
-    include Ably::Modules::StateEmitter
-    include Ably::Modules::UsesStateMachine
+# frozen_string_literal: true
 
-    attr_reader :state_machine
+module Ably
+  module Realtime
+    module Models
+      # Nil object for Channels, this object is only used within the internal API of this client library
+      # @api private
+      class NilChannel
+        include ::Ably::Modules::EventEmitter
+        extend ::Ably::Modules::Enum
+        STATE = ruby_enum('STATE', ::Ably::Realtime::Channel::STATE)
+        include ::Ably::Modules::StateEmitter
+        include ::Ably::Modules::UsesStateMachine
 
-    def initialize
-      @state_machine = Ably::Realtime::Channel::ChannelStateMachine.new(self)
-      @state         = STATE(state_machine.current_state)
-    end
+        attr_reader :state_machine
 
-    def name
-      'Nil channel'
-    end
+        def initialize
+          @state_machine = ::Ably::Realtime::Channel::ChannelStateMachine.new(self)
+          @state         = STATE(state_machine.current_state)
+        end
 
-    def __incoming_msgbus__
-      @__incoming_msgbus__ ||= Ably::Util::PubSub.new
-    end
+        def name
+          'Nil channel'
+        end
 
-    def logger
-      @logger ||= Ably::Models::NilLogger.new
+        def __incoming_msgbus__
+          @__incoming_msgbus__ ||= Ably::Util::PubSub.new
+        end
+
+        def logger
+          @logger ||= Ably::Models::NilLogger.new
+        end
+      end
     end
   end
 end
