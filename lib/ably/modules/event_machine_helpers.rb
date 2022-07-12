debeac23f9438375e2fc2996003708a821a8a68e
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:25:47 +0200

    Fix lib/ably/modules/event_machine_helpers.rb

--- lib/ably/modules/event_machine_helpers.rb
+++ lib/ably/modules/event_machine_helpers.rb
@@ -1,19 +1,23 @@
+# frozen_string_literal: true
+
 require 'eventmachine'
 
-module Ably::Modules
-  # EventMachineHelpers module provides common private methods to classes simplifying interaction with EventMachine
-  module EventMachineHelpers
-    private
+module Ably
+  module Modules
+    # EventMachineHelpers module provides common private methods to classes simplifying interaction with EventMachine
+    module EventMachineHelpers
+      private
 
-    # This method allows looped blocks to be run at the next EventMachine tick
-    # @example
-    #   x = 0
-    #   less_than_3 = -> { x < 3 }
-    #   non_blocking_loop_while(less_than_3) do
-    #     x += 1
-    #   end
-    def non_blocking_loop_while(lambda_condition, &execution_block)
-      if lambda_condition.call
+      # This method allows looped blocks to be run at the next EventMachine tick
+      # @example
+      #   x = 0
+      #   less_than_3 = -> { x < 3 }
+      #   non_blocking_loop_while(less_than_3) do
+      #     x += 1
+      #   end
+      def non_blocking_loop_while(lambda_condition, &execution_block)
+        return unless lambda_condition.call
+
         EventMachine.next_tick do
           if lambda_condition.call # ensure condition is still met following #next_tick
             yield
