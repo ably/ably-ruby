e594ea7d84170abf531ca651f047000e1851f3ad
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:17:52 +0200

    Fix lib/ably/rest/middleware/parse_json.rb

--- lib/ably/rest/middleware/parse_json.rb
+++ lib/ably/rest/middleware/parse_json.rb
@@ -1,22 +1,26 @@
+# frozen_string_literal: true
+
 require 'faraday'
 require 'json'
 
 module Ably
   module Rest
     module Middleware
+      # ParseJson provides the top-level class to be instanced for the Ably library
+      #
       class ParseJson < Faraday::Middleware
         def on_complete(env)
-          if env.response_headers['Content-Type'] == 'application/json'
-            env.body = parse(env.body) unless env.response_headers['Ably-Middleware-Parsed'] == true
-            env.response_headers['Ably-Middleware-Parsed'] = true
-          end
+          return unless env.response_headers['Content-Type'] == 'application/json'
+
+          env.body = parse(env.body) unless env.response_headers['Ably-Middleware-Parsed'] == true
+          env.response_headers['Ably-Middleware-Parsed'] = true
         end
 
         def parse(body)
-          if body.length > 0
-            JSON.parse(body)
-          else
+          if body.empty?
             body
+          else
+            JSON.parse(body)
           end
         rescue JSON::ParserError => e
           raise Ably::Exceptions::InvalidResponseBody, "Expected JSON response: #{e.message}"
