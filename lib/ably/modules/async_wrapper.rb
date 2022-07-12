c7544624c0284dc19e6439e89da98c27d918d1f3
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:27:23 +0200

    Fix lib/ably/modules/async_wrapper.rb

--- lib/ably/modules/async_wrapper.rb
+++ lib/ably/modules/async_wrapper.rb
@@ -1,67 +1,71 @@
+# frozen_string_literal: true
+
 require 'eventmachine'
 
-module Ably::Modules
-  # Provides methods to convert synchronous operations into async operations through the use of
-  # {http://www.rubydoc.info/github/eventmachine/eventmachine/EventMachine#defer-class_method EventMachine#defer}.
-  # The async_wrap method can only be called from within an EventMachine reactor, and must be thread safe.
-  #
-  # @note using this AsyncWrapper should only be used for methods that are used less frequently and typically
-  #       not run with levels of concurrency due to the limited number of threads available to EventMachine by default.
-  #       This module requires that the method #logger is defined.
-  #
-  # @example
-  #   class BlockingOperation
-  #     include Aby::Modules::AsyncWrapper
-  #
-  #     def operation(&success_callback)
-  #       async_wrap(success_callback) do
-  #         sleep 1
-  #         'slept'
-  #       end
-  #     end
-  #   end
-  #
-  #   blocking_object = BlockingOperation.new
-  #   deferrable = blocking_object.operation do |result|
-  #     puts "Done with result: #{result}"
-  #   end
-  #   puts "Starting"
-  #
-  #   # => 'Starting'
-  #   # => 'Done with result: slept'
-  #
-  module AsyncWrapper
-    private
-
-    # Will yield the provided block in a new thread and return an {Ably::Util::SafeDeferrable}
+module Ably
+  module Modules
+    # Provides methods to convert synchronous operations into async operations through the use of
+    # {http://www.rubydoc.info/github/eventmachine/eventmachine/EventMachine#defer-class_method EventMachine#defer}.
+    # The async_wrap method can only be called from within an EventMachine reactor, and must be thread safe.
     #
-    # @yield [Object] operation block that is run in a thread
-    # @return [Ably::Util::SafeDeferrable]
+    # @note using this AsyncWrapper should only be used for methods that are used less frequently and typically
+    #       not run with levels of concurrency due to the limited number of threads available to EventMachine by default.
+    #       This module requires that the method #logger is defined.
     #
-    def async_wrap(success_callback = nil, custom_error_handling = nil)
-      raise ArgumentError, 'Block required' unless block_given?
+    # @example
+    #   class BlockingOperation
+    #     include Aby::Modules::AsyncWrapper
+    #
+    #     def operation(&success_callback)
+    #       async_wrap(success_callback) do
+    #         sleep 1
+    #         'slept'
+    #       end
+    #     end
+    #   end
+    #
+    #   blocking_object = BlockingOperation.new
+    #   deferrable = blocking_object.operation do |result|
+    #     puts "Done with result: #{result}"
+    #   end
+    #   puts "Starting"
+    #
+    #   # => 'Starting'
+    #   # => 'Done with result: slept'
+    #
+    module AsyncWrapper
+      private
 
-      Ably::Util::SafeDeferrable.new(logger).tap do |deferrable|
-        deferrable.callback(&success_callback) if success_callback
+      # Will yield the provided block in a new thread and return an {Ably::Util::SafeDeferrable}
+      #
+      # @yield [Object] operation block that is run in a thread
+      # @return [Ably::Util::SafeDeferrable]
+      #
+      def async_wrap(success_callback = nil, custom_error_handling = nil)
+        raise ArgumentError, 'Block required' unless block_given?
 
-        operation_with_exception_handling = lambda do
-          begin
-            yield
-          rescue StandardError => err
-            if custom_error_handling
-              custom_error_handling.call err, deferrable
-            else
-              logger.error { "An exception in an AsyncWrapper block was caught. #{err.class}: #{err.message}\n#{err.backtrace.join("\n")}" }
-              deferrable.fail err
+        Ably::Util::SafeDeferrable.new(logger).tap do |deferrable|
+          deferrable.callback(&success_callback) if success_callback
+
+          operation_with_exception_handling = lambda do
+            begin
+              yield
+            rescue StandardError => e
+              if custom_error_handling
+                custom_error_handling.call e, deferrable
+              else
+                logger.error { "An exception in an AsyncWrapper block was caught. #{err.class}: #{err.message}\n#{err.backtrace.join("\n")}" }
+                deferrable.fail e
+              end
             end
           end
-        end
 
-        complete_callback = lambda do |result|
-          deferrable.succeed result
-        end
+          complete_callback = lambda do |result|
+            deferrable.succeed result
+          end
 
-        EventMachine.defer operation_with_exception_handling, complete_callback
+          EventMachine.defer operation_with_exception_handling, complete_callback
+        end
       end
     end
   end
