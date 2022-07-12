22fabbd7d9394a62f5a6f218f1f20d44509b3ad0
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:32:05 +0200

    Fix lib/ably/models/delta_extras.rb

--- lib/ably/models/delta_extras.rb
+++ lib/ably/models/delta_extras.rb
@@ -1,29 +1,33 @@
-module Ably::Models
-  #
-  # @!attribute [r] from
-  #   @return [String] The id of the message the delta was generated from
-  # @!attribute [r] format
-  #   @return [String] The delta format. Only vcdiff is supported as at API version 1.2
-  #
-  class DeltaExtras
-    include Ably::Modules::ModelCommon
+# frozen_string_literal: true
 
-    # The id of the message the delta was generated from.
-    # @return [String, nil]
+module Ably
+  module Models
     #
-    attr_reader :from
-
-    # The delta format.
-    # @return [String, nil]
+    # @!attribute [r] from
+    #   @return [String] The id of the message the delta was generated from
+    # @!attribute [r] format
+    #   @return [String] The delta format. Only vcdiff is supported as at API version 1.2
     #
-    attr_reader :format
+    class DeltaExtras
+      include ::Ably::Modules::ModelCommon
 
-    def initialize(attributes = {})
-      @from, @format = IdiomaticRubyWrapper((attributes || {}), stop_at: [:from, :format]).attributes.values_at(:from, :format)
-    end
+      # The id of the message the delta was generated from.
+      # @return [String, nil]
+      #
+      attr_reader :from
 
-    def to_json(*args)
-      as_json(args).to_json
+      # The delta format.
+      # @return [String, nil]
+      #
+      attr_reader :format
+
+      def initialize(attributes = {})
+        @from, @format = IdiomaticRubyWrapper((attributes || {}), stop_at: %I[from format]).attributes.values_at(:from, :format)
+      end
+
+      def to_json(*args)
+        as_json(args).to_json
+      end
     end
   end
 end
