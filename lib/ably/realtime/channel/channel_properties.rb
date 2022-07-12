9266bf8c085caccc83279622ea23709bd18431b5
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:23:18 +0200

    Fix lib/ably/realtime/channel/channel_properties.rb

--- lib/ably/realtime/channel/channel_properties.rb
+++ lib/ably/realtime/channel/channel_properties.rb
@@ -1,23 +1,27 @@
-module Ably::Realtime
-  class Channel
-    # Represents properties of a channel and its state
-    class ChannelProperties
-      # {Ably::Realtime::Channel} this object associated with
-      # @return [Ably::Realtime::Channel]
-      attr_reader :channel
+# frozen_string_literal: true
 
-      # Contains the last channelSerial received in an ATTACHED ProtocolMesage for the channel, see RTL15a
-      #
-      # @return [String]
-      attr_reader :attach_serial
+module Ably
+  module Realtime
+    class Channel
+      # Represents properties of a channel and its state
+      class ChannelProperties
+        # {Ably::Realtime::Channel} this object associated with
+        # @return [Ably::Realtime::Channel]
+        attr_reader :channel
 
-      def initialize(channel)
-        @channel = channel
-      end
+        # Contains the last channelSerial received in an ATTACHED ProtocolMesage for the channel, see RTL15a
+        #
+        # @return [String]
+        attr_reader :attach_serial
 
-      # @api private
-      def set_attach_serial(attach_serial)
-        @attach_serial = attach_serial
+        def initialize(channel)
+          @channel = channel
+        end
+
+        # @api private
+        def set_attach_serial(attach_serial)
+          @attach_serial = attach_serial
+        end
       end
     end
   end
