85f01ff2dcc864e33d47bd4a2058a735af62a623
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-12 16:15:24 +0200

    Fix lib/ably/util/safe_deferrable.rb

--- lib/ably/util/safe_deferrable.rb
+++ lib/ably/util/safe_deferrable.rb
@@ -1,42 +1,46 @@
-module Ably::Util
-  # SafeDeferrable class provides a Deferrable that is safe to use for for public interfaces
-  # of this client library.  Any exceptions raised in the success or failure callbacks are
-  # caught and logged to the provided logger.
-  #
-  # An exception in a callback provided by a developer should not break this client library
-  # and stop further execution of code.
-  #
-  class SafeDeferrable
-    include Ably::Modules::SafeDeferrable
+# frozen_string_literal: true
 
-    attr_reader :logger
+module Ably
+  module Util
+    # SafeDeferrable class provides a Deferrable that is safe to use for for public interfaces
+    # of this client library.  Any exceptions raised in the success or failure callbacks are
+    # caught and logged to the provided logger.
+    #
+    # An exception in a callback provided by a developer should not break this client library
+    # and stop further execution of code.
+    #
+    class SafeDeferrable
+      include ::Ably::Modules::SafeDeferrable
 
-    def initialize(logger)
-      @logger = logger
-    end
+      attr_reader :logger
 
-    # Create a new {SafeDeferrable} and fail immediately with the provided error in the next eventloop cycle
-    #
-    # @param error [Ably::Exceptions::BaseAblyException, Ably::Models::ErrorInfo]   The error used to fail the newly created {SafeDeferrable}
-    #
-    # @return [SafeDeferrable]
-    #
-    def self.new_and_fail_immediately(logger, error)
-      new(logger).tap do |deferrable|
-        EventMachine.next_tick do
-          deferrable.fail error
+      def initialize(logger)
+        @logger = logger
+      end
+
+      # Create a new {SafeDeferrable} and fail immediately with the provided error in the next eventloop cycle
+      #
+      # @param error [Ably::Exceptions::BaseAblyException, Ably::Models::ErrorInfo]   The error used to fail the newly created {SafeDeferrable}
+      #
+      # @return [SafeDeferrable]
+      #
+      def self.new_and_fail_immediately(logger, error)
+        new(logger).tap do |deferrable|
+          EventMachine.next_tick do
+            deferrable.fail error
+          end
         end
       end
-    end
 
-    # Create a new {SafeDeferrable} and succeed immediately with the provided arguments in the next eventloop cycle
-    #
-    # @return [SafeDeferrable]
-    #
-    def self.new_and_succeed_immediately(logger, *args)
-      new(logger).tap do |deferrable|
-        EventMachine.next_tick do
-          deferrable.succeed *args
+      # Create a new {SafeDeferrable} and succeed immediately with the provided arguments in the next eventloop cycle
+      #
+      # @return [SafeDeferrable]
+      #
+      def self.new_and_succeed_immediately(logger, *args)
+        new(logger).tap do |deferrable|
+          EventMachine.next_tick do
+            deferrable.succeed(*args)
+          end
         end
       end
     end
