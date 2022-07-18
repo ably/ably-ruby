f4e830a1ffb53aa9399ef71398c1049f76319d60
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-18 18:14:33 +0200

    Fix lib/ably/modules/ably.rb

--- lib/ably/modules/ably.rb
+++ lib/ably/modules/ably.rb
@@ -10,15 +10,15 @@ module Ably
   # network failures either at the client, between the client and Ably, within an Ably data center, or at the IO domain registrar
   # see https://ably.com/docs/client-lib-development-guide/features/#RSC15a
   #
-  FALLBACK_DOMAIN = 'ably-realtime.com'.freeze
+  FALLBACK_DOMAIN = 'ably-realtime.com'
   FALLBACK_IDS = %w[a b c d e].freeze
 
   # Default production fallbacks a.ably-realtime.com ... e.ably-realtime.com
-  FALLBACK_HOSTS = FALLBACK_IDS.map { |host| "#{host}.#{FALLBACK_DOMAIN}".freeze }.freeze
+  FALLBACK_HOSTS = FALLBACK_IDS.map { |host| "#{host}.#{FALLBACK_DOMAIN}" }.freeze
 
   # Custom environment default fallbacks {ENV}-a-fallback.ably-realtime.com ... {ENV}-a-fallback.ably-realtime.com
   CUSTOM_ENVIRONMENT_FALLBACKS_SUFFIXES = FALLBACK_IDS.map do |host|
-    "-#{host}-fallback.#{FALLBACK_DOMAIN}".freeze
+    "-#{host}-fallback.#{FALLBACK_DOMAIN}"
   end.freeze
 
   INTERNET_CHECK = {
