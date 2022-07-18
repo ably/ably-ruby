65e615ce02d5eddae347a8ba10ae4ce9b70c88e5
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-07-18 18:16:18 +0200

    Fix lib/ably/models/device_details.rb

--- lib/ably/models/device_details.rb
+++ lib/ably/models/device_details.rb
@@ -45,8 +45,8 @@ module Ably
       include ::Ably::Modules::ModelCommon
 
       # @param hash_object   [Hash,nil]  Device detail attributes
-      #a
       def initialize(hash_object = {})
+        super
         @raw_hash_object = hash_object || {}
         @hash_object     = IdiomaticRubyWrapper(hash_object)
       end
