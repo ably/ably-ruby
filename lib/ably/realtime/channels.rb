2914fb8005896e9b87cf7df882d9dc2324b52317
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-09-12 16:53:01 +0200

    Fix lib/ably/realtime/channels.rb descriptions

--- lib/ably/realtime/channels.rb
+++ lib/ably/realtime/channels.rb
@@ -5,28 +5,29 @@ module Ably
       include Ably::Modules::ChannelsCollection
 
       # @return [Ably::Realtime::Channels]
+      #
       def initialize(client)
         super client, Ably::Realtime::Channel
       end
 
-      # @!method get(name, channel_options = {})
       # Return a {Ably::Realtime::Channel} for the given name
       #
       # @param name [String] The name of the channel
       # @param channel_options [Hash, Ably::Models::ChannelOptions] A hash of options or a {Ably::Models::ChannelOptions}
-      # @return [Ably::Realtime::Channel}
+      #
+      # @return [Ably::Realtime::Channel]
       #
       def get(*args)
         super
       end
 
-      # @!method fetch(name, &missing_block)
       # Return a {Ably::Realtime::Channel} for the given name if it exists, else the block will be called.
       # This method is intentionally similar to {http://ruby-doc.org/core-2.1.3/Hash.html#method-i-fetch Hash#fetch} providing a simple way to check if a channel exists or not without creating one
       #
       # @param name [String] The name of the channel
       # @yield [options] (optional) if a missing_block is passed to this method and no channel exists matching the name, this block is called
       # @yieldparam [String] name of the missing channel
+      #
       # @return [Ably::Realtime::Channel]
       #
       def fetch(*args)
