90af70df57f340767b1a826c54598b6c5e1bad82
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-09-12 16:51:44 +0200

    Fix lib/ably/realtime/push.rb descriptions

--- lib/ably/realtime/push.rb
+++ lib/ably/realtime/push.rb
@@ -11,27 +11,39 @@ module Ably
         @client = client
       end
 
-      # Admin features for push notifications like managing devices and channel subscriptions
+      # A {Ably::Realtime::Push::Admin} object.
+      #
+      # @spec RSH1
+      #
       # @return [Ably::Realtime::Push::Admin]
+      #
       def admin
         @admin ||= Admin.new(self)
       end
 
-      # Activate this device for push notifications by registering with the push transport such as GCM/APNS
+      # Activates the device for push notifications with FCM or APNS, obtaining a unique identifier from them.
+      # Subsequently registers the device with Ably and stores the deviceIdentityToken in local storage.
+      #
+      # @spec RSH2a
       #
       # @note This is unsupported in the Ruby library
+      #
       def activate(*arg)
         raise_unsupported
       end
 
-      # Deactivate this device for push notifications by removing the registration with the push transport such as GCM/APNS
+      # Deactivates the device from receiving push notifications with Ably and FCM or APNS.
+      #
+      # @spec RSH2b
       #
       # @note This is unsupported in the Ruby library
+      #
       def deactivate(*arg)
         raise_unsupported
       end
 
       private
+
       def raise_unsupported
         raise Ably::Exceptions::PushNotificationsNotSupported, 'This device does not support receiving or subscribing to push notifications. All PushChannel methods are unavailable'
       end
