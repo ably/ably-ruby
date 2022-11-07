9e12f7ad8e2efefa547281a7bf58b18898b91461
Łukasz Śliwa <lukasz.sliwa@gmail.com>
2022-11-07 13:29:52 +0100

    Fix spec/unit/realtime/connection_spec.rb

--- spec/unit/realtime/connection_spec.rb
+++ spec/unit/realtime/connection_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'shared/protocol_msgbus_behaviour'
 
 describe Ably::Realtime::Connection do
-  let(:client) { instance_double('Ably::Realtime::Client', logger: double('logger').as_null_object, recover: nil, endpoint: double('endpoint', host: 'realtime.ably.io')) }
+  let(:client) { instance_double('Ably::Realtime::Client', logger: double('logger').as_null_object, recover: nil, endpoint: double('endpoint', host: 'realtime.ably.io'), client_id: '123') }
 
   subject do
     Ably::Realtime::Connection.new(client, {}).tap do |connection|
@@ -33,6 +33,68 @@ describe Ably::Realtime::Connection do
 
   it_behaves_like 'an incoming protocol message bus'
   it_behaves_like 'an outgoing protocol message bus'
+
+  describe '#recovery_key' do
+    context 'when key is empty' do
+      before { allow(subject).to receive(:key).and_return nil }
+
+      it 'should return nil' do
+        expect(subject.recovery_key).to be_nil
+      end
+    end
+
+    context 'when connection is closing' do
+      it 'should return nil' do
+        allow(subject).to receive(:key).and_return nil
+        allow(subject).to receive(:closing?).and_return(true)
+        expect(subject.recovery_key).to be_nil
+      end
+    end
+
+    context 'when connection is closed' do
+      it 'should return nil' do
+        allow(subject).to receive(:key).and_return 'present'
+        allow(subject).to receive(:closing?).and_return(false)
+        allow(subject).to receive(:closed?).and_return(true)
+        expect(subject.recovery_key).to be_nil
+      end
+    end
+
+    context 'when connection is failed' do
+      it 'should return nil' do
+        allow(subject).to receive(:key).and_return 'present'
+        allow(subject).to receive(:closing?).and_return(false)
+        allow(subject).to receive(:closed?).and_return(false)
+        allow(subject).to receive(:failed?).and_return(true)
+        expect(subject.recovery_key).to be_nil
+      end
+    end
+
+    context 'when connection is suspended' do
+      it 'should return nil' do
+        allow(subject).to receive(:key).and_return 'present'
+        allow(subject).to receive(:closing?).and_return(false)
+        allow(subject).to receive(:closed?).and_return(false)
+        allow(subject).to receive(:failed?).and_return(false)
+        allow(subject).to receive(:suspended?).and_return(true)
+        expect(subject.recovery_key).to be_nil
+      end
+    end
+
+    context 'when key is present and connection is not closing/closed/failed/suspended' do
+      it 'should return JSON with connectionKey, msgSerial and channelSerials' do
+        allow(subject).to receive(:key).and_return 'present'
+        allow(subject).to receive(:closing?).and_return(false)
+        allow(subject).to receive(:closed?).and_return(false)
+        allow(subject).to receive(:failed?).and_return(false)
+        allow(subject).to receive(:suspended?).and_return(false)
+        allow(subject.client).to receive(:connection).and_return(subject)
+        channels = [['channel1', '1234'], ['channel2', '1235'], ['channel3', '1236']]
+        allow(subject.client).to receive(:serials).and_return(Hash[channels])
+        expect(subject.recovery_key).to eq("{\"connectionKey\":\"present\",\"msgSerial\":0,\"channelSerials\":{\"channel1\":\"1234\",\"channel2\":\"1235\",\"channel3\":\"1236\"}}")
+      end
+    end
+  end
 
   describe 'connection resume callbacks', api_private: true do
     let(:callbacks) { [] }
