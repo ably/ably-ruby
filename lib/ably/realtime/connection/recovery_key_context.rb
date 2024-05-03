1d0f95931031f213587b6456d808c0d24814bd01
sacOO7 <sachinshinde7676@gmail.com>
2024-05-03 16:26:17 +0530

    Added a separate class for recoveryKeyContext

---
+++ lib/ably/realtime/connection/recovery_key_context.rb
@@ -0,0 +1,21 @@
+require 'json'
+# frozen_string_literal: true
+
+module Ably::Realtime
+  class RecoveryKeyContext
+    attr_reader :connection_key
+    attr_reader :msg_serial
+    attr_reader :channel_serials
+
+    def initialize(connection_key, msg_serial, channel_serials)
+      @connection_key = connection_key
+      @msg_serial = msg_serial
+      @channel_serials = channel_serials
+    end
+
+    def encode
+      JSON.generate(self)
+    end
+
+  end
+end
