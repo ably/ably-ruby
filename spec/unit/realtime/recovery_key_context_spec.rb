631b1f838681aa8ac71d1db3937b1ab772e5a2a9
sacOO7 <sachinshinde7676@gmail.com>
2024-05-07 17:44:48 +0530

    Updated recovery key context tests for decoding recovery key

--- spec/unit/realtime/recovery_key_context_spec.rb
+++ spec/unit/realtime/recovery_key_context_spec.rb
@@ -3,21 +3,28 @@ require 'ably/realtime/recovery_key_context'
 
 describe Ably::Realtime::RecoveryKeyContext do
 
-  context 'recovery context' do
-    let(:recovery_context) do
+  context 'connection recovery key' do
+
+    it 'should encode recovery key' do
       connection_key = 'key'
       msg_serial = 123
       channel_serials = {
         'channel1' => 'serial1',
         'channel2' => 'serial2'
       }
-      return Ably::Realtime::RecoveryKeyContext.new(connection_key, msg_serial, channel_serials)
+      recovery_context = Ably::Realtime::RecoveryKeyContext.new(connection_key, msg_serial, channel_serials)
+      encoded_recovery_key = recovery_context.to_json
+      expect(encoded_recovery_key).to eq "{\"connection_key\":\"key\",\"msg_serial\":123," <<
+                                                "\"channel_serials\":{\"channel1\":\"serial1\",\"channel2\":\"serial2\"}}"
     end
 
-    it 'should encode recovery key context' do
-      encoded_recovery_key = recovery_context.to_json
-      expect(encoded_recovery_key).to eql "{\"connection_key\":\"key\",\"msg_serial\":123," <<
-                                            "\"channel_serials\":{\"channel1\":\"serial1\",\"channel2\":\"serial2\"}}"
+    it 'should decode recovery key' do
+      encoded_recovery_key = "{\"connection_key\":\"key\",\"msg_serial\":123," <<
+                                     "\"channel_serials\":{\"channel1\":\"serial1\",\"channel2\":\"serial2\"}}"
+      decoded_recovery_key = Ably::Realtime::RecoveryKeyContext.from_json(encoded_recovery_key)
+      expect(decoded_recovery_key.connection_key).to eq("key")
+      expect(decoded_recovery_key.msg_serial).to eq(123)
     end
+
   end
 end
