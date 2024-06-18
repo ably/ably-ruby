4889cc4dfb0d2dc373a7ffc7f0aec2606302c746
sacOO7 <sachinshinde7676@gmail.com>
2024-06-18 23:23:10 +0530

    reverted need to make sure cliets connected for setup_test

--- spec/acceptance/realtime/presence_spec.rb
+++ spec/acceptance/realtime/presence_spec.rb
@@ -41,23 +41,21 @@ describe Ably::Realtime::Presence, :event_machine do
       end
 
       def setup_test(method_name, args, options)
-        client_one.connection.once :connected do
-          if options[:enter_first]
-            acked = false
-            received = false
-            presence_client_one.public_send(method_name.to_s.gsub(/leave|update/, 'enter'), args) do
-              acked = true
-              yield if acked & received
-            end
-            presence_client_one.subscribe do |presence_message|
-              expect(presence_message.action).to eq(:enter)
-              presence_client_one.unsubscribe
-              received = true
-              yield if acked & received
-            end
-          else
-            yield
+        if options[:enter_first]
+          acked = false
+          received = false
+          presence_client_one.public_send(method_name.to_s.gsub(/leave|update/, 'enter'), args) do
+            acked = true
+            yield if acked & received
           end
+          presence_client_one.subscribe do |presence_message|
+            expect(presence_message.action).to eq(:enter)
+            presence_client_one.unsubscribe
+            received = true
+            yield if acked & received
+          end
+        else
+          yield
         end
       end
 
